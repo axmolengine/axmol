5faf15e10fa1f94b9bec488a4d98818ae68cf87a
halx99 <halx99@live.com>
2019-11-25 17:06:01 +0800

    Use toString instead.

--- cocos/platform/CCSAXParser.cpp
+++ cocos/platform/CCSAXParser.cpp
@@ -3,7 +3,6 @@
  Copyright (c) 2010 cocos2d-x.org  
  Copyright (c) 2013 Martell Malone
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- Copyright (c) 2019-2020 simdsoft.com, @HALX99
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
