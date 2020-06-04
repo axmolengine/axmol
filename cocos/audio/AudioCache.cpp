3a75843131bf1ed704cbbcea47bbd30a78992a9f
halx99 <halx99@live.com>
2020-06-04 12:55:22 +0800

    Remove unused macro

--- cocos/audio/AudioCache.cpp
+++ cocos/audio/AudioCache.cpp
@@ -50,8 +50,6 @@ unsigned int __idIndex = 0;
 #define INVALID_AL_BUFFER_ID 0xFFFFFFFF
 #define PCMDATA_CACHEMAXSIZE 1048576
 
-#define CC_ALIGN(d, a) (((d) + ((a)-1)) & ~((a)-1))
-
 using namespace cocos2d;
 
 AudioCache::AudioCache()
