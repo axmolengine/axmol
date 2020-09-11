378975d768547fe114ea0ab42911a7f9a2a2c4c8
halx99 <halx99@live.com>
2020-09-11 12:39:16 +0800

    Remove comment

--- cocos/2d/CCRenderTexture.cpp
+++ cocos/2d/CCRenderTexture.cpp
@@ -488,29 +488,6 @@ void RenderTexture::newImage(std::function<void(RefPtr<Image>)> imageCallback, b
         else imageCallback(nullptr);
     };
     _director->getRenderer()->addCommand(&_captureCommand);
-
-//    Image *image = new (std::nothrow) Image();
-//    if (image) {
-//        _texture2D->getBackendTexture()->getBytes(0, 0, savedBufferWidth, savedBufferHeight, flipImage, [=](const unsigned char* tempData, size_t, size_t) {
-//            image->initWithRawData(tempData, savedBufferWidth * savedBufferHeight * 4, savedBufferWidth, savedBufferHeight, 8, hasPremultipliedAlpha);
-//            imageCallback(image);
-//            });
-//    }
-    
-//    do
-//    {
-////        // TODO: move this to configuration, so we don't check it every time
-////        /*  Certain Qualcomm Adreno GPU's will retain data in memory after a frame buffer switch which corrupts the render to the texture. The solution is to clear the frame buffer before rendering to the texture. However, calling glClear has the unintended result of clearing the current texture. Create a temporary texture to overcome this. At the end of RenderTexture::begin(), switch the attached texture to the second one, call glClear, and then switch back to the original texture. This solution is unnecessary for other devices as they don't have the same issue with switching frame buffers.
-////         */
-////        if (Configuration::getInstance()->checkForGLExtension("GL_QCOM"))
-////        {
-////            // -- bind a temporary texture so we can clear the render buffer without losing our texture
-////            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _textureCopy->getName(), 0);
-////            CHECK_GL_ERROR_DEBUG();
-////            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-////            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _texture->getName(), 0);
-////        }
-//    } while (0);
 }
 
 void RenderTexture::draw(Renderer *renderer, const Mat4 &transform, uint32_t flags)
