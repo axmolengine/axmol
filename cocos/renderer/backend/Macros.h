c79da1ca0520beabb827cd16a94508804bbba17b
halx99 <halx99@live.com>
2020-09-11 14:45:45 +0800

    fix ci

--- cocos/renderer/backend/Macros.h
+++ cocos/renderer/backend/Macros.h
@@ -30,3 +30,19 @@
 #define MAX_COLOR_ATTCHMENT 1
 
 #define MAX_INFLIGHT_BUFFER 3
+
+ /*
+  * helps the compiler's optimizer predicting branches
+  */
+#if __has_builtin(__builtin_expect)
+#   ifdef __cplusplus
+#      define UTILS_LIKELY( exp )    (__builtin_expect( !!(exp), true ))
+#      define UTILS_UNLIKELY( exp )  (__builtin_expect( !!(exp), false ))
+#   else
+#      define UTILS_LIKELY( exp )    (__builtin_expect( !!(exp), 1 ))
+#      define UTILS_UNLIKELY( exp )  (__builtin_expect( !!(exp), 0 ))
+#   endif
+#else
+#   define UTILS_LIKELY( exp )    (!!(exp))
+#   define UTILS_UNLIKELY( exp )  (!!(exp))
+#endif
