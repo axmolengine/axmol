0d433503d7f40273f1a19a15d1e84f051c5e74bc
HALX99 <halx99@live.com>
2020-10-07 13:51:23 +0800

    Update before-install.sh

--- tools/travis-scripts/before-install.sh
+++ tools/travis-scripts/before-install.sh
@@ -55,7 +55,7 @@ function install_environement_for_pull_request()
         
         sudo apt-get install build-essential checkinstall
         sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-        wget https://www.python.org/ftp/python/$version/Python-2.7.12.tgz
+        wget https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tgz
         tar -xvf Python-2.7.12.tgz
         cd Python-2.7.12
         ./configure
