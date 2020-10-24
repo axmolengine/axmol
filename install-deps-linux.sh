c9538b2981937c7e713f5a71911434430e0379f6
halx99 <halx99@live.com>
2020-10-24 10:37:38 +0800

    Temp disable gcc 32

--- install-deps-linux.sh
+++ install-deps-linux.sh
@@ -14,10 +14,10 @@ sudo apt-get update
 # run 32bit applicatio: needed for lua relase mode as luajit has 32bit version
 # https://askubuntu.com/questions/454253/how-to-run-32-bit-app-in-ubuntu-64-bit
 sudo dpkg --add-architecture i386
-DEPENDS='libc6:i386 libncurses5:i386 libstdc++6:i386'
+# DEPENDS='libc6:i386 libncurses5:i386 libstdc++6:i386'
  
 
-DEPENDS+=' libx11-dev'
+DEPENDS=' libx11-dev'
 DEPENDS+=' libxmu-dev'
 DEPENDS+=' libglu1-mesa-dev'
 DEPENDS+=' libgl2ps-dev'
@@ -33,4 +33,4 @@ DEPENDS+=' libgtk-3-dev'
 DEPENDS+=' binutils'
 DEPENDS+=' libbsd-dev'
 
-sudo apt-get install --force-yes --yes $DEPENDS > /dev/null
+sudo apt-get install --allow --yes $DEPENDS > /dev/null
