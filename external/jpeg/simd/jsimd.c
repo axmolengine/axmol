82ee7e7c618829b2d1ce0d4f1cae12f0e466be9c
halx99 <halx99@live.com>
2020-12-03 15:30:44 +0800

    fix android build for libjpeg-turbo [ci build]

--- external/jpeg/simd/jsimd.c
+++ external/jpeg/simd/jsimd.c
@@ -1,8 +1,6 @@
 // By halx99, This file is only workaround for build ios combined archs
-#if defined(__APPLE__)
-    #if defined(__arm__)
-        #include "arm/jsimd.c"
-    #elif defined(__arm64__)
-        #include "arm64/jsimd.c"
-    #endif
+#if defined(__arm__)
+    #include "arm/jsimd.c"
+#elif defined(__arm64__)
+    #include "arm64/jsimd.c"
 #endif
