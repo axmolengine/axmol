1238a08662f2de1e0186593f999d74c297812b74
rh101 <rh101@users.noreply.github.com>
2021-04-25 02:39:47 +1000

    Add support for FileStream in ZipUtils and ZipFile

--- cocos/base/ZipUtils.h
+++ cocos/base/ZipUtils.h
@@ -29,6 +29,8 @@ THE SOFTWARE.
 #define __SUPPORT_ZIPUTILS_H__
 /// @cond DO_NOT_SHOW
 
+#include <ioapi.h>
+
 #include "platform/CCPlatformMacros.h"
 #include "platform/CCFileUtils.h"
 #include <string>
@@ -316,6 +318,7 @@ namespace cocos2d
         CC_DEPRECATED() static unsigned char* getFileDataFromZip(const std::string& zipFilePath, const std::string& filename, ssize_t* size);
         
     private:
+        void fillFunctionOverrides();
         /* Only used internal for createWithBuffer() */
         ZipFile();
         
@@ -324,6 +327,7 @@ namespace cocos2d
         
         /** Internal data like zip file pointer / file list array and so on */
         ZipFilePrivate *_data;
+        zlib_filefunc_def _functionOverrides;
     };
 } // end of namespace cocos2d
 
