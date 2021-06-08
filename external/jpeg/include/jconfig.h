c45592ba79fe0ff114730e3fa5dd6b4cf4e1e8c0
halx99 <halx99@live.com>
2021-06-08 19:23:17 +0800

    Retry ci [ci build]

--- external/jpeg/include/jconfig.h
+++ external/jpeg/include/jconfig.h
@@ -6,36 +6,8 @@
 
 #if defined(_WIN32)
 #  include "win32/jconfig.h"
-
-#elif defined(__APPLE__)
-#  include <TargetConditionals.h>
-
-#  if TARGET_IPHONE_SIMULATOR == 1
-#    include "ios-x64/jconfig.h"
-#  elif TARGET_OS_IPHONE == 1
-#    if defined(__arm64__)
-#      include "ios-arm64/jconfig.h"
-#    elif defined(__arm__)
-#      include "ios-arm/jconfig.h"
-#    endif
-#  elif TARGET_OS_MAC == 1
-#    include "mac/jconfig.h"
-#  endif
-
-#elif defined(__ANDROID__)
-
-#  if defined(__aarch64__) || defined(__arm64__)
-#    include "android-arm64/jconfig.h"
-#  elif defined(__arm__)
-#    include "android-arm/jconfig.h"
-#  else
-#    include "android-x86/jconfig.h"
-#  endif
-
-#elif defined(__linux__)
-#  include "linux/jconfig.h"
 #else
-#  error "unsupported platform"
+#  include "unix/jconfig.h"
 #endif
 
 #endif
