00d7e86b9a29d4ac3f0f85a46ac7928cb26d9f22
halx99 <halx99@live.com>
2021-06-24 00:20:39 +0800

    Update url

--- cocos/base/ConcurrentDeque.h
+++ cocos/base/ConcurrentDeque.h
@@ -2,7 +2,7 @@
 *
  Copyright (c) 2021 Bytedance Inc.
 
- http://www.adxe.org
+ https://adxe.org
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
