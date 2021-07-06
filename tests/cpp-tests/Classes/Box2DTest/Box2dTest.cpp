43cd8cf3bc587ae8dceab2be35ede3c056bf0664
aismann <icesoft@freenet.de>
2021-07-06 09:47:25 +0200

    Update Box2dTest.cpp

--- tests/cpp-tests/Classes/Box2DTest/Box2dTest.cpp
+++ tests/cpp-tests/Classes/Box2DTest/Box2dTest.cpp
@@ -22,7 +22,7 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-#include "Box2DTest.h"
+#include "Box2dTest.h"
 #include "../testResource.h"
 #include "extensions/cocos-ext.h"
 #include "renderer/CCRenderer.h"
