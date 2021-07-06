5fb1982b18bd3427529437dee8b8b3e04c3d97c6
halx99 <halx99@live.com>
2021-07-06 14:36:28 +0800

    Update doxgen config files

--- docs/MainPage.h
+++ docs/MainPage.h
@@ -1,31 +1,25 @@
 /*!
 @cond GEN_REFERENCE_BOOK
 
-@mainpage cocos2d-x
+@mainpage adxe
 
 @image html cocos2dx_portrait.png
 
-@section sec1 About cocos2d-x
-cocos2d-x open source project is designed to be a cross-platform 2D game engine for building 2D games, demos and other graphical/interactive mobile applications.
+@section sec1 About adxe
+adxe is based on cocos2d-x-4.0, cocos2d-x is a open source project is designed to be a cross-platform 2D game engine for building 2D games, demos and other graphical/interactive mobile applications.
 It runs on top of OpenGL 2.0 and OpenGL ES 2.0 and is written in C++.\n
 This project is based on the famous <A HREF="http://www.cocos2d-iphone.org">"cocos2d-iphone"</A> project.\n
 
-- website:                          http://www.cocos2d-x.org/
-- forum:                            http://forum.cocos2d-x.org/
-- twitter:                          http://twitter.com/cocos2dx/
-- weibo:                            http://weibo.com/cocos2dx/
-- wiki:                             http://wiki.cocos2d-x.org/
-- show cases:                       http://www.cocos2d-x.org/games
+- website:                          https://adxe.org/
 
-- <A HREF="http://www.cocos2d-x.org/projects/cocos2d-x/wiki/Tutorials">Tutorials of Cocos2dxSimpleGame</A>
-- <A HREF="http://www.cocos2d-x.org/projects/cocos2d-x/wiki/Installation_and_First_run">Installation and First run</A>
-- <A HREF="http://www.cocos2d-x.org/boards/6/topics/567">FAQ</A>
+- <A HREF="https://github.com/adxeproject/adxe/issues">FAQ</A>
 
 \n
 
 @section sec2 License
 Copyright (c) 2010-2012 cocos2d-x.org \n
-Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd. \n
+Copyright (c) 2021 Bytedance Inc. \n
 \n
 Permission is hereby granted, free of charge, to any person obtaining a copy \n
 of this software and associated documentation files (the "Software"), to deal \n
@@ -47,10 +41,9 @@ THE SOFTWARE. \n
 \n
 
 @section sec3 Get the source code
-- Stable version: \n
-    - publish at http://download.cocos2d-x.org/
+
 - Latest code: \n
-    - http://github.com/cocos2d/cocos2d-x/
+    - https://github.com/adxeproject/adxe
 
 @section sec4 How to contribute
 - Fork our repository on github, commit your changes, and send a "pull request" to us. We will merge your contribution to master
