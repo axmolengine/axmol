50c270e8f54e77ef03b627dee7ef0e42bea5da60
halx99 <halx99@live.com>
2021-08-06 19:40:32 +0800

    Merge: 0a682f0dc1 c2b44cc7e1 Merge pull request #445 from aismann/Box2D-Basic-Clean-code-and-speed-optimized
    Box2D - Basic ready for use (clean code and speed optimized)

--- tests/cpp-tests/Classes/Box2DTestBed/Test.h
+++ tests/cpp-tests/Classes/Box2DTestBed/Test.h
@@ -51,53 +51,53 @@ inline float RandomFloat(float lo, float hi)
 }
 
 /// Test settings. Some can be controlled in the GUI.
-struct Settings
-{
-	Settings()
-	{
-		viewCenter.Set(0.0f, 20.0f);
-		hz = 60.0f;
-		velocityIterations = 8;
-		positionIterations = 3;
-		drawShapes = 1;
-		drawJoints = 1;
-		drawAABBs = 0;
-		drawContactPoints = 0;
-		drawContactNormals = 0;
-		drawContactImpulse = 0;
-		drawFrictionImpulse = 0;
-		drawCOMs = 0;
-		drawStats = 0;
-		drawProfile = 0;
-		enableWarmStarting = 1;
-		enableContinuous = 1;
-		enableSubStepping = 0;
-		enableSleep = 1;
-		pause = 0;
-		singleStep = 0;
-	}
-
-	b2Vec2 viewCenter;
-	float hz;
-	int32 velocityIterations;
-	int32 positionIterations;
-	int32 drawShapes;
-	int32 drawJoints;
-	int32 drawAABBs;
-	int32 drawContactPoints;
-	int32 drawContactNormals;
-	int32 drawContactImpulse;
-	int32 drawFrictionImpulse;
-	int32 drawCOMs;
-	int32 drawStats;
-	int32 drawProfile;
-	int32 enableWarmStarting;
-	int32 enableContinuous;
-	int32 enableSubStepping;
-	int32 enableSleep;
-	int32 pause;
-	int32 singleStep;
-};
+//struct Settings
+//{
+//	Settings()
+//	{
+//		viewCenter.Set(0.0f, 20.0f);
+//		hz = 60.0f;
+//		velocityIterations = 8;
+//		positionIterations = 3;
+//		drawShapes = 1;
+//		drawJoints = 1;
+//		drawAABBs = 0;
+//		drawContactPoints = 0;
+//		drawContactNormals = 0;
+//		drawContactImpulse = 0;
+//		drawFrictionImpulse = 0;
+//		drawCOMs = 0;
+//		drawStats = 0;
+//		drawProfile = 0;
+//		enableWarmStarting = 1;
+//		enableContinuous = 1;
+//		enableSubStepping = 0;
+//		enableSleep = 1;
+//		pause = 0;
+//		singleStep = 0;
+//	}
+//
+//	b2Vec2 viewCenter;
+//	float hz;
+//	int32 velocityIterations;
+//	int32 positionIterations;
+//	int32 drawShapes;
+//	int32 drawJoints;
+//	int32 drawAABBs;
+//	int32 drawContactPoints;
+//	int32 drawContactNormals;
+//	int32 drawContactImpulse;
+//	int32 drawFrictionImpulse;
+//	int32 drawCOMs;
+//	int32 drawStats;
+//	int32 drawProfile;
+//	int32 enableWarmStarting;
+//	int32 enableContinuous;
+//	int32 enableSubStepping;
+//	int32 enableSleep;
+//	int32 pause;
+//	int32 singleStep;
+//};
 
 
 // This is called when a joint in the world is implicitly destroyed
@@ -113,18 +113,18 @@ public:
 };
 
 const int32 k_maxContactPoints = 2048;
-
-struct ContactPoint
-{
-	b2Fixture* fixtureA;
-	b2Fixture* fixtureB;
-	b2Vec2 normal;
-	b2Vec2 position;
-	b2PointState state;
-	float normalImpulse;
-	float tangentImpulse;
-	float separation;
-};
+//
+//struct ContactPoint
+//{
+//	b2Fixture* fixtureA;
+//	b2Fixture* fixtureB;
+//	b2Vec2 normal;
+//	b2Vec2 position;
+//	b2PointState state;
+//	float normalImpulse;
+//	float tangentImpulse;
+//	float separation;
+//};
 class Test : public b2ContactListener
 {
 public:
