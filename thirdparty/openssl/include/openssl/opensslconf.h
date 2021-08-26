547835b3deadb797a4a15cb112e56da79e70ea10
halx99 <halx99@live.com>
2021-08-26 23:57:36 +0800

    Add win64 libs

--- thirdparty/openssl/include/openssl/opensslconf.h
+++ thirdparty/openssl/include/openssl/opensslconf.h
@@ -5,37 +5,42 @@
 #define BUILDWARE_openssl_REDIRECT_H
 
 #if defined(_WIN32)
-#  include "win32/openssl/opensslconf.h"
+
+#    if defined(_WIN64)
+#        include "win64/openssl/opensslconf.h"
+#    else
+#        include "win32/openssl/opensslconf.h"
+#    endif
 
 #elif defined(__APPLE__)
-#  include <TargetConditionals.h>
+#    include <TargetConditionals.h>
 
-#  if TARGET_IPHONE_SIMULATOR == 1
-#    include "ios-x64/openssl/opensslconf.h"
-#  elif TARGET_OS_IPHONE == 1
-#    if defined(__arm64__)
-#      include "ios-arm64/openssl/opensslconf.h"
-#    elif defined(__arm__)
-#      include "ios-arm/openssl/opensslconf.h"
+#    if TARGET_IPHONE_SIMULATOR == 1
+#        include "ios-x64/openssl/opensslconf.h"
+#    elif TARGET_OS_IPHONE == 1
+#        if defined(__arm64__)
+#            include "ios-arm64/openssl/opensslconf.h"
+#        elif defined(__arm__)
+#            include "ios-arm/openssl/opensslconf.h"
+#        endif
+#    elif TARGET_OS_MAC == 1
+#        include "mac/openssl/opensslconf.h"
 #    endif
-#  elif TARGET_OS_MAC == 1
-#    include "mac/openssl/opensslconf.h"
-#  endif
 
 #elif defined(__ANDROID__)
 
-#  if defined(__aarch64__) || defined(__arm64__)
-#    include "android-arm64/openssl/opensslconf.h"
-#  elif defined(__arm__)
-#    include "android-arm/openssl/opensslconf.h"
-#  else
-#    include "android-x86/openssl/opensslconf.h"
-#  endif
+#    if defined(__aarch64__) || defined(__arm64__)
+#        include "android-arm64/openssl/opensslconf.h"
+#    elif defined(__arm__)
+#        include "android-arm/openssl/opensslconf.h"
+#    else
+#        include "android-x86/openssl/opensslconf.h"
+#    endif
 
 #elif defined(__linux__)
-#  include "linux/openssl/opensslconf.h"
+#    include "linux/openssl/opensslconf.h"
 #else
-#  error "unsupported platform"
+#    error "unsupported platform"
 #endif
 
 #endif
