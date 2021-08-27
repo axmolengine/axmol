63cfa97024b41124441259aaceb42dfe3ed84a02
halx99 <halx99@live.com>
2021-08-27 08:02:03 +0800

    Update build.ps1

--- tools/windows-ci/build.ps1
+++ tools/windows-ci/build.ps1
@@ -6,9 +6,6 @@ $CONFIG_ALL_OPTIONS=@()
 if($BUILD_ARCH -eq "x86") {
    $CONFIG_ALL_OPTIONS += '-A', 'Win32'
 }
-else {
-   $CONFIG_ALL_OPTIONS += '-A', 'Win64'
-}
 
 cmake -S . -B build_$BUILD_ARCH $CONFIG_ALL_OPTIONS -DBUILD_EXTENSION_IMGUIEXT=ON
 
