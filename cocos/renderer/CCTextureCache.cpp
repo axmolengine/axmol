ab2aa262420bc21fdf6e3820c2d660188300fae2
rh101 <rh101@users.noreply.github.com>
2021-09-02 22:43:10 +1000

    Change %lu to %u

--- cocos/renderer/CCTextureCache.cpp
+++ cocos/renderer/CCTextureCache.cpp
@@ -652,7 +652,7 @@ std::string TextureCache::getCachedTextureInfo() const
         auto bytes = tex->getPixelsWide() * tex->getPixelsHigh() * bpp / 8;
         totalBytes += bytes;
         count++;
-        snprintf(buftmp, sizeof(buftmp) - 1, "\"%s\" rc=%lu id=%p %lu x %lu @ %d bpp => %lu KB\n",
+        snprintf(buftmp, sizeof(buftmp) - 1, "\"%s\" rc=%u id=%p %u x %u @ %d bpp => %u KB\n",
             texture.first.c_str(),
             (int32_t)tex->getReferenceCount(),
             tex->getBackendTexture(),
