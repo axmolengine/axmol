c9159c62fa1a9c65d96e05ca00efec6b07255786
halx99 <halx99@live.com>
2021-12-08 00:15:08 +0800

    Delete unused binding generator

--- tools/bindings-generator/targets/spidermonkey/templates/function.h
+++
@@ -1,1 +0,0 @@
-bool js_${generator.prefix}_${class_name}_${func_name}(JSContext *cx, uint32_t argc, jsval *vp);
--- tools/bindings-generator/targets/spidermonkey/templates/prelude.c
+++
@@ -1,4 +0,0 @@
-#set generator = $current_class.generator
-JSClass  *jsb_${current_class.underlined_class_name}_class;
-JSObject *jsb_${current_class.underlined_class_name}_prototype;
-
