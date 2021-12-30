369de318531ebcdd5b168bf8e65b3dfbe4fec927
halx99 <halx99@live.com>
2021-12-30 21:24:22 +0800

    Tidy tools

--- tools/jenkins-scripts/slave-scripts/framework/mac-build.sh
+++
@@ -1,5 +0,0 @@
-#put xctool.sh into your PATH
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ADXE_ROOT="$DIR"/../../../..
-cd ${ADXE_ROOT}
-python tools/framework-compile/bin/gen_cocos_libs.py -c --mac | xcpretty
--- tools/jenkins-scripts/slave-scripts/tests/android-build.sh
+++
@@ -1,5 +0,0 @@
-#put xctool.sh into your PATH
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ADXE_ROOT="$DIR"/../../../..
-cd ${ADXE_ROOT}
-python build/android-build.py -p 13 cpp-tests lua-tests js-tests
