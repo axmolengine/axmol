1e1644da37bfc7abfe668a0e9ef357c06d15651c
halx99 <halx99@live.com>
2022-02-24 20:34:24 +0800

    Update readme & copyright year

--- docs/MainPage.h
+++ docs/MainPage.h
@@ -19,7 +19,7 @@ This project is based on the famous <A HREF="http://www.cocos2d-iphone.org">"coc
 @section sec2 License
 Copyright (c) 2010-2012 cocos2d-x.org \n
 Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd. \n
-Copyright (c) 2021 Bytedance Inc. \n
+Copyright (c) 2021-2022 Bytedance Inc. \n
 \n
 Permission is hereby granted, free of charge, to any person obtaining a copy \n
 of this software and associated documentation files (the "Software"), to deal \n
