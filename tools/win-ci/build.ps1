e8f6ea09e7143a68ad70ee3c3bc9ef57ecd065c3
一线灵 <halx99@live.com>
2022-04-24 20:08:48 +0800

    Try fix windows ci

--- tools/win-ci/build.ps1
+++ tools/win-ci/build.ps1
@@ -7,7 +7,7 @@ if($BUILD_ARCH -eq "x86") {
    $CONFIG_ALL_OPTIONS += '-A', 'Win32'
 }
 
-cmake -S . -B build_$BUILD_ARCH $CONFIG_ALL_OPTIONS -DAX_ENABLE_EXT_IMGUI=ON
+cmake -S . -B build_$BUILD_ARCH $CONFIG_ALL_OPTIONS -Thost=x64 -DAX_ENABLE_EXT_IMGUI=ON
 
 cmake --build build_$BUILD_ARCH --config Release --target cpp-tests
 cmake --build build_$BUILD_ARCH --config Release --target lua-tests
