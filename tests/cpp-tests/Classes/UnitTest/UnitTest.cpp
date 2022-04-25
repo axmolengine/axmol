4c9d1dfd00c4c01e82d2871bd7608ff61e0134f8
halx99 <halx99@live.com>
2022-04-25 12:06:43 +0800

    Update copyright url [skip ci]

--- tests/cpp-tests/Classes/UnitTest/UnitTest.cpp
+++ tests/cpp-tests/Classes/UnitTest/UnitTest.cpp
@@ -2,7 +2,7 @@
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
  Copyright (c) 2021 Bytedance Inc.
 
- https://adxe.org
+ https://adxeproject.github.io/
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
