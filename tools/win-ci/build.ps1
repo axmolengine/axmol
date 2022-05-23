d0b551d26b823ae4cf54fc3087fee78b867482ba
Turky Mohammed <45469625+DelinWorks@users.noreply.github.com>
2022-05-23 16:55:30 +0300

    Merge: 6e1d48835e 193ed82788 Merge branch 'adxeproject:dev' into partical-system-animation

--- tools/win-ci/build.ps1
+++ tools/win-ci/build.ps1
@@ -9,5 +9,5 @@ if($BUILD_ARCH -eq "x86") {
 
 cmake -S . -B build_$BUILD_ARCH $CONFIG_ALL_OPTIONS -Thost=x64 -DAX_ENABLE_EXT_IMGUI=ON
 
-cmake --build build_$BUILD_ARCH --config Release --target cpp-tests
-cmake --build build_$BUILD_ARCH --config Release --target lua-tests
+cmake --build build_$BUILD_ARCH --config Release --target cpp_tests
+cmake --build build_$BUILD_ARCH --config Release --target lua_tests
