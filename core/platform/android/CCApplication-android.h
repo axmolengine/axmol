6d2bc9bbf20309e48b61c42a1b30e0587cbf1e35
涓€绾跨伒 <halx99@live.com>
2022-06-30 21:04:00 +0800

    Revert "Unify frame rate control logical to director mainLoop"

--- core/platform/android/CCApplication-android.h
+++ core/platform/android/CCApplication-android.h
@@ -2,7 +2,6 @@
 Copyright (c) 2010-2012 cocos2d-x.org
 Copyright (c) 2013-2016 Chukong Technologies Inc.
 Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
-Copyright (c) 2022 Bytedance Inc.
 
 https://adxeproject.github.io/
 
@@ -43,6 +42,12 @@ public:
      * @lua NA
      */
     virtual ~Application();
+
+    /**
+    @brief    Callback by Director to limit FPS.
+    @param interval The time, expressed in seconds, between current frame and next.
+    */
+    virtual void setAnimationInterval(float interval) override;
 
     /**
     @brief    Run the message loop.
