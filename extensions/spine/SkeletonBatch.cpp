db7681bb8b6586e167d04d60d9485f226110913b
halx99 <halx99@live.com>
2022-07-05 14:36:04 +0800

    [Spine] Use backend shader definations

--- extensions/spine/SkeletonBatch.cpp
+++ extensions/spine/SkeletonBatch.cpp
@@ -96,17 +96,17 @@ backend::ProgramState* SkeletonBatch::updateCommandPipelinePS(SkeletonCommand* c
 		currentState = programState->clone();
 		
 		auto vertexLayout = currentState->getVertexLayout();
-		auto locPosition = currentState->getAttributeLocation("a_position");
-		auto locTexcoord = currentState->getAttributeLocation("a_texCoord");
-		auto locColor = currentState->getAttributeLocation("a_color");
-		vertexLayout->setAttribute("a_position", locPosition, backend::VertexFormat::FLOAT3, offsetof(V3F_C4B_T2F, vertices), false);
-		vertexLayout->setAttribute("a_color", locColor, backend::VertexFormat::UBYTE4, offsetof(V3F_C4B_T2F, colors), true);
-		vertexLayout->setAttribute("a_texCoord", locTexcoord, backend::VertexFormat::FLOAT2, offsetof(V3F_C4B_T2F, texCoords), false);
+		auto locPosition = programState->getAttributeLocation(backend::ATTRIBUTE_NAME_POSITION);
+		auto locTexcoord = programState->getAttributeLocation(backend::ATTRIBUTE_NAME_TEXCOORD);
+		auto locColor = programState->getAttributeLocation(backend::ATTRIBUTE_NAME_COLOR);
+		vertexLayout->setAttribute(backend::ATTRIBUTE_NAME_POSITION, locPosition, backend::VertexFormat::FLOAT3, offsetof(V3F_C4B_T2F, vertices), false);
+		vertexLayout->setAttribute(backend::ATTRIBUTE_NAME_COLOR, locColor, backend::VertexFormat::UBYTE4, offsetof(V3F_C4B_T2F, colors), true);
+		vertexLayout->setAttribute(backend::ATTRIBUTE_NAME_TEXCOORD, locTexcoord, backend::VertexFormat::FLOAT2, offsetof(V3F_C4B_T2F, texCoords), false);
 		vertexLayout->setLayout(sizeof(_vertices[0]));
 
 
-		command->_locMVP = currentState->getUniformLocation("u_MVPMatrix");
-		command->_locTexture = currentState->getUniformLocation(backend::UNIFORM_NAME_TEXTURE);
+		command->_locMVP = programState->getUniformLocation(backend::UNIFORM_NAME_MVP_MATRIX);
+        command->_locTexture = programState->getUniformLocation(backend::UNIFORM_NAME_TEXTURE);
 	}
 	return currentState;
 }
