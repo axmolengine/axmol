6116cac44103be268400d3e7ae99c5d465701918
halx99 <halx99@live.com>
2022-10-11 22:34:47 +0800

    Improve code style

--- core/renderer/CCRenderer.cpp
+++ core/renderer/CCRenderer.cpp
@@ -147,7 +147,7 @@ void RenderQueue::realloc(size_t reserveSize)
 {
     for (int i = 0; i < QUEUE_GROUP::QUEUE_COUNT; ++i)
     {
-        _commands[i] = std::vector<RenderCommand*>();
+        _commands[i].clear();
         _commands[i].reserve(reserveSize);
     }
 }
@@ -166,8 +166,7 @@ Renderer::Renderer()
 
     _commandGroupStack.push(DEFAULT_RENDER_QUEUE);
 
-    RenderQueue defaultRenderQueue;
-    _renderGroups.emplace_back(defaultRenderQueue);
+    _renderGroups.emplace_back();
     _queuedTriangleCommands.reserve(BATCH_TRIAGCOMMAND_RESERVED_SIZE);
 
     // for the batched TriangleCommand
