03d8945362d0f142c4d6f97edb832b77bc3b49ec
halx99 <halx99@live.com>
2022-10-27 13:01:18 +0800

    Fix ci warnings

--- thirdparty/bullet/BulletCollision/NarrowPhaseCollision/btGjkConvexCast.cpp
+++ thirdparty/bullet/BulletCollision/NarrowPhaseCollision/btGjkConvexCast.cpp
@@ -4,8 +4,8 @@ Copyright (c) 2003-2006 Erwin Coumans  https://bulletphysics.org
 
 This software is provided 'as-is', without any express or implied warranty.
 In no event will the authors be held liable for any damages arising from the use of this software.
-Permission is granted to anyone to use this software for any purpose, 
-including commercial applications, and to alter it and redistribute it freely, 
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it freely,
 subject to the following restrictions:
 
 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
@@ -19,10 +19,8 @@ subject to the following restrictions:
 #include "btPointCollector.h"
 #include "LinearMath/btTransformUtil.h"
 
-#ifdef BT_USE_DOUBLE_PRECISION
+#ifdef MAX_ITERATIONS
 #define MAX_ITERATIONS 64
-#else
-#define MAX_ITERATIONS 32
 #endif
 
 btGjkConvexCast::btGjkConvexCast(const btConvexShape* convexA, const btConvexShape* convexB, btSimplexSolverInterface* simplexSolver)
