445e51d4c4b052b2d6988cee30e2bd021ad8a829
halx99 <halx99@users.noreply.github.com>
2022-12-07 01:31:10 +0000

    Committing genbindings changes

--- extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
+++ extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
@@ -10192,6 +10192,53 @@ int lua_ax_base_Node_getChildByTag(lua_State* tolua_S)
 
     return 0;
 }
+int lua_ax_base_Node_getWorldPosition(lua_State* tolua_S)
+{
+    int argc = 0;
+    ax::Node* cobj = nullptr;
+    bool ok  = true;
+
+#if _AX_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if _AX_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"ax.Node",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    cobj = (ax::Node*)tolua_tousertype(tolua_S,1,0);
+
+#if _AX_DEBUG >= 1
+    if (!cobj) 
+    {
+        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_ax_base_Node_getWorldPosition'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 0) 
+    {
+        if(!ok)
+        {
+            tolua_error(tolua_S,"invalid arguments in function 'lua_ax_base_Node_getWorldPosition'", nullptr);
+            return 0;
+        }
+        auto&& ret = cobj->getWorldPosition();
+        vec2_to_luaval(tolua_S, ret);
+        return 1;
+    }
+    luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d \n", "ax.Node:getWorldPosition",argc, 0);
+    return 0;
+
+#if _AX_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_ax_base_Node_getWorldPosition'.",&tolua_err);
+#endif
+
+    return 0;
+}
 int lua_ax_base_Node_getScaleZ(lua_State* tolua_S)
 {
     int argc = 0;
@@ -12123,6 +12170,7 @@ int lua_register_ax_base_Node(lua_State* tolua_S)
         tolua_function(tolua_S,"setGlobalZOrder",lua_ax_base_Node_setGlobalZOrder);
         tolua_function(tolua_S,"setScale",lua_ax_base_Node_setScale);
         tolua_function(tolua_S,"getChildByTag",lua_ax_base_Node_getChildByTag);
+        tolua_function(tolua_S,"getWorldPosition",lua_ax_base_Node_getWorldPosition);
         tolua_function(tolua_S,"getScaleZ",lua_ax_base_Node_getScaleZ);
         tolua_function(tolua_S,"getScaleY",lua_ax_base_Node_getScaleY);
         tolua_function(tolua_S,"getScaleX",lua_ax_base_Node_getScaleX);
