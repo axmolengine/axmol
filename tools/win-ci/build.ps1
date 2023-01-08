100a688ce5083da54d22dda0685d269cd24038f2
一线灵|Deal <halx99@live.com>
2023-01-08 22:04:14 +0800

    Add windows dll build checks for GH actions (#1023)
    * Update windows-ci.yml
    
    * Update pr-ci.yml
    
    * Update build.ps1
    
    * Update windows-ci.yml
    
    * Update build.ps1

--- tools/win-ci/build.ps1
+++ tools/win-ci/build.ps1
@@ -1,5 +1,5 @@
 $BUILD_ARCH = $args[0]
-$WINSDK_VER = $args[1]
+$BUILD_DLL = $args[1]
 
 # CONFIG_ALL_OPTIONS
 $CONFIG_ALL_OPTIONS=@()
@@ -8,12 +8,16 @@ if ($BUILD_ARCH -eq "x86") {
     $CONFIG_ALL_OPTIONS += '-A', 'Win32'
 }
 
+if ($BUILD_DLL -eq "true") {
+    $CONFIG_ALL_OPTIONS += '-DBUILD_SHARED_LIBS=TRUE'
+}
+
 # windows sdk version, require xxx, but 10.0.22621.0 recommanded
 
-if ($WINSDK_VER -eq '') { # empty, cmake will choose same with runner windows OS build version
+if ($env:WINSDK_VER -eq '') { # empty, cmake will choose same with runner windows OS build version
     cmake -S . -B build_$BUILD_ARCH $CONFIG_ALL_OPTIONS -Thost=x64 -DAX_ENABLE_EXT_IMGUI=TRUE -DAX_ENABLE_EXT_LIVE2D=TRUE -DAX_ENABLE_EXT_EFFEKSEER=TRUE
 } else {
-    cmake -S . -B build_$BUILD_ARCH $CONFIG_ALL_OPTIONS -Thost=x64 -DAX_ENABLE_EXT_IMGUI=TRUE -DAX_ENABLE_EXT_LIVE2D=TRUE -DAX_ENABLE_EXT_EFFEKSEER=TRUE "-DCMAKE_SYSTEM_VERSION=$WINSDK_VER"
+    cmake -S . -B build_$BUILD_ARCH $CONFIG_ALL_OPTIONS -Thost=x64 -DAX_ENABLE_EXT_IMGUI=TRUE -DAX_ENABLE_EXT_LIVE2D=TRUE -DAX_ENABLE_EXT_EFFEKSEER=TRUE "-DCMAKE_SYSTEM_VERSION=$env:WINSDK_VER"
 }
 
 cmake --build build_$BUILD_ARCH --config Release --target cpp_tests
