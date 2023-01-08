b04c8f5735c991056263a14bcd0a03cb1fd6c451
Turky Mohammed <45469625+DelinWorks@users.noreply.github.com>
2023-01-08 04:36:43 +0300

    [BUG] fix _AX_HAVE_WEBVIEW2 compilation error when not defined. (#1020) * make _AX_HAVE_WEBVIEW2 actually work
    * Exclude other platforms
    
    * Update UIWebView-inl.h

--- core/ui/UIWebView/UIWebView-inl.h
+++ core/ui/UIWebView/UIWebView-inl.h
@@ -26,6 +26,8 @@
 
 /// @cond DO_NOT_SHOW
 
+#if (defined(_WIN32) && defined(_AX_HAVE_WEBVIEW2)) || (AX_TARGET_PLATFORM == AX_PLATFORM_ANDROID || AX_TARGET_PLATFORM == AX_PLATFORM_IOS)
+
 #include "ui/UIWebView/UIWebView.h"
 #include "platform/CCGLView.h"
 #include "base/CCDirector.h"
@@ -236,5 +238,7 @@ WebView::ccWebViewCallback WebView::getOnJSCallback() const
 
 }  // namespace ui
 NS_AX_END  // namespace ax
+
+#endif
 
 /// @endcond
