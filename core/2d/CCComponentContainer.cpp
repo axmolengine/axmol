181ee677fc9b1530ffb921fbc8661eae558fdc79
Turky Mohammed <45469625+DelinWorks@users.noreply.github.com>
2023-02-09 17:25:11 +0300

    Update CCComponentContainer.cpp

--- core/2d/CCComponentContainer.cpp
+++ core/2d/CCComponentContainer.cpp
@@ -57,7 +57,7 @@ bool ComponentContainer::add(Component* com)
 
         if (_componentMap.find(componentName) != _componentMap.end())
         {
-            AXASSERT(false, "ComponentContainer already have this kind of component");
+            //AXASSERT(false, "ComponentContainer already have this kind of component");
             break;
         }
         hlookup::set_item(_componentMap, componentName, com);  //_componentMap[componentName] = com;
