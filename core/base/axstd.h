2d8d754cd961db9eaaf0b5dfeb01b581d63efa1a
halx99 <halx99@live.com>
2023-03-10 13:20:45 +0800

    Remove range-v3 which is not compatible UWP c++17

--- core/base/axstd.h
+++ core/base/axstd.h
@@ -2,6 +2,7 @@
 
 #include <type_traits>
 #include <memory>
+#include <string_view>
 #include "pod_vector.h"
 
 // Tests whether compiler has c++23 support
@@ -14,24 +15,6 @@
 #endif      // C++23 features check
 #if !defined(_AX_HAS_CXX23)
 #    define _AX_HAS_CXX23 0
-#endif
-
-#if !_AX_HAS_CXX23
-#    include "range/v3/range.hpp"
-#    include "range/v3/view.hpp"
-namespace axstd
-{
-using namespace ::ranges;
-namespace ranges
-{
-using namespace ::ranges;
-}
-}  // namespace axstd
-#else
-namespace axstd
-{
-using namespace std;
-}
 #endif
 
 namespace axstd
@@ -77,4 +60,31 @@ inline pod_vector<_Ty> pod_vector_from(const _InIt _First, const _InIt _Last, _F
     std::transform(_First, _Last, dest.begin(), _Func);
     return dest;
 }
+
+template <typename _CStr, typename _Fn>
+inline void split_cb(_CStr s, size_t slen, typename std::remove_pointer<_CStr>::type delim, _Fn&& func)
+{
+    auto _Start = s;  // the start of every string
+    auto _Ptr   = s;  // source string iterator
+    auto _End   = s + slen;
+    while ((_Ptr = strchr(_Ptr, delim)))
+    {
+        if (_Ptr >= _End)
+            break;
+
+        if (_Start <= _Ptr)
+            func(_Start, _Ptr);
+        _Start = _Ptr + 1;
+        ++_Ptr;
+    }
+    if (_Start <= _End)
+        func(_Start, _End);
+}
+
+template <typename _Fn>
+inline void split_cb(std::string_view s, char delim, _Fn&& func)
+{
+    split_cb(s.data(), s.length(), delim, std::move(func));
+}
+
 }  // namespace axstd
