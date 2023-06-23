413c9fcdc98291286a8e5d95cd6697714e25dd91
halx99 <halx99@live.com>
2023-06-24 00:41:34 +0800

    Remove unused

--- thirdparty/recast/sync.ps1
+++ thirdparty/recast/sync.ps1
@@ -13,3 +13,8 @@ Copy-Item "$recast_root/DetourCrowd/Source/*.cpp" "$my_root/"
 
 Copy-Item "$recast_root/DetourTileCache/Include/*.h" "$my_root"
 Copy-Item "$recast_root/DetourTileCache/Source/*.cpp" "$my_root"
+
+Remove-Item -Path "$my_root/RecastDump.h"
+Remove-Item -Path "$my_root/RecastDump.cpp"
+Remove-Item -Path "$my_root/RecastDebugDraw.h"
+Remove-Item -Path "$my_root/RecastDebugDraw.cpp"
