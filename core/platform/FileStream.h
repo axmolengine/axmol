6b58db0eb2fc1829c2598cca0e20f3f27aace2eb
halx99 <halx99@live.com>
2023-08-11 15:25:19 +0800

    Move posix defines to FileStream.cpp

--- core/platform/FileStream.h
+++ core/platform/FileStream.h
@@ -9,32 +9,6 @@
 
 #include "platform/PlatformMacros.h"
 
-#if !defined(_WIN32)
-#    include <unistd.h>
-#    include <errno.h>
-#    include <fcntl.h>
-#    define O_READ_FLAGS O_RDONLY, 0
-#    define O_WRITE_FLAGS O_CREAT | O_RDWR | O_TRUNC, S_IRWXU
-#    define O_APPEND_FLAGS O_APPEND | O_CREAT | O_RDWR, S_IRWXU
-#    define O_OVERLAP_FLAGS O_CREAT | O_RDWR, S_IRWXU
-
-#    define posix_open_cxx(path, ...) ::open(path.data(), ##__VA_ARGS__)
-#    define posix_open ::open
-#    define posix_close ::close
-#    define posix_lseek ::lseek
-#    define posix_read ::read
-#    define posix_write ::write
-#    define posix_fd2fh(fd) (fd)
-#    define posix_ftruncate ::ftruncate
-#    if defined(__APPLE__)
-#        define posix_lseek64 ::lseek
-#        define posix_ftruncate64 ::ftruncate
-#    else
-#        define posix_lseek64 ::lseek64
-#        define posix_ftruncate64 ::ftruncate64
-#    endif
-#endif
-
 #if AX_TARGET_PLATFORM == AX_PLATFORM_ANDROID
 #    include <android/asset_manager.h>
 #endif
