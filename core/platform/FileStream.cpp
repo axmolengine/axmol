071dc846a2f2ea50122fa6ab6df94f3978f254e4
halx99 <halx99@live.com>
2023-08-11 15:25:19 +0800

    Move posix defines to FileStream.cpp

--- core/platform/FileStream.cpp
+++ core/platform/FileStream.cpp
@@ -12,6 +12,30 @@
 
 #if defined(_WIN32)
 #    include "ntcvt/ntcvt.hpp"
+#else
+#    include <unistd.h>
+#    include <errno.h>
+#    include <fcntl.h>
+#    define O_READ_FLAGS O_RDONLY, 0
+#    define O_WRITE_FLAGS O_CREAT | O_RDWR | O_TRUNC, S_IRWXU
+#    define O_APPEND_FLAGS O_APPEND | O_CREAT | O_RDWR, S_IRWXU
+#    define O_OVERLAP_FLAGS O_CREAT | O_RDWR, S_IRWXU
+
+#    define posix_open_cxx(path, ...) ::open(path.data(), ##__VA_ARGS__)
+#    define posix_open ::open
+#    define posix_close ::close
+#    define posix_lseek ::lseek
+#    define posix_read ::read
+#    define posix_write ::write
+#    define posix_fd2fh(fd) (fd)
+#    define posix_ftruncate ::ftruncate
+#    if defined(__APPLE__)
+#        define posix_lseek64 ::lseek
+#        define posix_ftruncate64 ::ftruncate
+#    else
+#        define posix_lseek64 ::lseek64
+#        define posix_ftruncate64 ::ftruncate64
+#    endif
 #endif
 
 NS_AX_BEGIN
