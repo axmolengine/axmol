1d26aa28a5114bbcca60b6d013f6848a5c90ef60
halx99 <halx99@live.com>
2023-08-31 21:20:01 +0800

    Improve axmol site build

--- tools/ci/gendocs.ps1
+++ tools/ci/gendocs.ps1
@@ -10,6 +10,12 @@ if (!(Test-Path "$tools_dir" -PathType Container)) {
     mkdir $tools_dir
 }
 
+function mkdirs([string]$path) {
+    if (!(Test-Path $path)) {
+        New-Item $path -ItemType Directory 1>$null
+    }
+}
+
 function setup_doxygen() {
     $doxygen_ver = '1.9.7'
 
@@ -48,31 +54,31 @@ setup_doxygen
 Write-Host "Using doxygen $(doxygen --version)"
 
 $AX_ROOT = (Resolve-Path $DIR/../..)
-$axver_file = (Resolve-Path $AX_ROOT/core/axmolver.h.in).Path
-$content = ($(Get-Content -Path $axver_file) | Select-String 'AX_VERSION_STR')
-$axver = $content[0].Line.Split(' ')[2].Replace('"', '')
 
-$git_prog = (Get-Command 'git' -ErrorAction SilentlyContinue).Source
-if($git_prog) {
-    Write-Host "Found git: $git_prog"
-    $branchName = $(git -C $AX_ROOT branch --show-current)
-    if ($branchName -eq 'dev') {
-        $commitHash = $(git -C $AX_ROOT rev-parse --short=7 HEAD)
-        $axver += "-$commitHash"
+function query_axmol_latest() {
+    $axver_file = (Resolve-Path $AX_ROOT/core/axmolver.h.in).Path
+    $content = ($(Get-Content -Path $axver_file) | Select-String 'AX_VERSION_STR')
+    $axver = $content[0].Line.Split(' ')[2].Replace('"', '')
+
+    $git_prog = (Get-Command 'git' -ErrorAction SilentlyContinue).Source
+    if($git_prog) {
+        Write-Host "Found git: $git_prog"
+        $branchName = $(git -C $AX_ROOT branch --show-current)
+        if ($branchName -eq 'dev') {
+            $commitHash = $(git -C $AX_ROOT rev-parse --short=7 HEAD)
+            $axver += "-$commitHash"
+        }
     }
+    return $axver
 }
 
-$docsRoot = (Resolve-Path "$DIR/../../docs").Path
+$site_src = (Resolve-Path "$DIR/../../docs").Path
+$site_dist = Join-Path $site_src 'dist'
+
+mkdirs $site_dist
 
 $store_cwd = (Get-Location).Path
-Set-Location $docsRoot
-
-
-function mkdirs([string]$path) {
-    if (!(Test-Path $path)) {
-        New-Item $path -ItemType Directory 1>$null
-    }
-}
+Set-Location $site_src
 
 function  configure_file($infile, $outfile, $vars) {
     $content = $(Get-Content $infile -raw)
@@ -84,38 +90,41 @@ function  configure_file($infile, $outfile, $vars) {
 
 # building latest
 $verMap = @{
-    'latest' = "v$axver"
+    'latest' = $null;
     '1.0' = "v1.0.0"
 }
 
 $strVerList = "'$($verMap.Keys -join "','")'"
 
-mkdirs './out'
-
 foreach($item in $verMap.GetEnumerator()) {
     $ver = $item.Key
-    $html_out_base = "./out/$ver"
-    mkdirs $html_out_base
+    $html_out = Join-Path $site_dist "manual/$ver"
+    mkdirs $html_out
     $release_tag = $item.Value
 
     if ($ver -eq 'latest') {
         git checkout dev
+        $release_tag = query_axmol_latest
     } elseif($ver -eq '1.0') {
         git checkout '1.x' # 1.x branch now for v1.0
     } else {
         git checkout $release_tag
     }
-    configure_file './Doxyfile.in' './Doxyfile' @{'@VERSION@'=$release_tag; '@HTML_OUTPUT@' = "$ver/manual"}
+    configure_file './Doxyfile.in' './Doxyfile' @{'@VERSION@'=$release_tag; '@HTML_OUTPUT@' = "manual/$ver"}
 
     doxygen "./Doxyfile"
 
-    $html_out = Join-Path $html_out_base 'manual'
     Copy-Item './hacks.js' $html_out
-    Copy-Item './stylesheet.css' $html_out
+    Copy-Item './doc_style.css' "$html_out/stylesheet.css"
     configure_file './menu_version.js.in' "$html_out/menu_version.js" @{'@VERLIST@' = $strVerList; '@VERSION@' = $ver}
 }
 
 # set default doc ver to 'latest'
-configure_file './index.html.in' "./out/index.html" @{'@VERSION@' = 'latest'}
+configure_file './doc_index.html.in' "$site_dist/manual/index.html" @{'@VERSION@' = 'latest'}
+
+# build home site
+mkdirs "$site_dist/assets/css"
+Copy-Item './style.css'  "$site_dist/assets/css/style.css"
+Copy-Item './index.html' "$site_dist/index.html"
 
 Set-Location $store_cwd
