012e614d5aaa4f5c302f72ec1a6dc15271588886
halx99 <halx99@users.noreply.github.com>
2023-09-19 13:53:04 +0000

    Committing genbindings changes

--- extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
+++ extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
@@ -55445,6 +55445,103 @@ int lua_ax_base_Label_getMaxLineWidth(lua_State* tolua_S)
 
     return 0;
 }
+int lua_ax_base_Label_setTTFFaceSize(lua_State* tolua_S)
+{
+    int argc = 0;
+    ax::Label* cobj = nullptr;
+    bool ok  = true;
+
+#if _AX_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if _AX_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"ax.Label",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    cobj = (ax::Label*)tolua_tousertype(tolua_S,1,0);
+
+#if _AX_DEBUG >= 1
+    if (!cobj) 
+    {
+        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_ax_base_Label_setTTFFaceSize'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 1) 
+    {
+        int arg0;
+
+        ok &= luaval_to_int32(tolua_S, 2,(int *)&arg0, "ax.Label:setTTFFaceSize");
+        if(!ok)
+        {
+            tolua_error(tolua_S,"invalid arguments in function 'lua_ax_base_Label_setTTFFaceSize'", nullptr);
+            return 0;
+        }
+        cobj->setTTFFaceSize(arg0);
+        lua_settop(tolua_S, 1);
+        return 1;
+    }
+    luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d \n", "ax.Label:setTTFFaceSize",argc, 1);
+    return 0;
+
+#if _AX_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_ax_base_Label_setTTFFaceSize'.",&tolua_err);
+#endif
+
+    return 0;
+}
+int lua_ax_base_Label_getTTFFaceSize(lua_State* tolua_S)
+{
+    int argc = 0;
+    ax::Label* cobj = nullptr;
+    bool ok  = true;
+
+#if _AX_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if _AX_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"ax.Label",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    cobj = (ax::Label*)tolua_tousertype(tolua_S,1,0);
+
+#if _AX_DEBUG >= 1
+    if (!cobj) 
+    {
+        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_ax_base_Label_getTTFFaceSize'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 0) 
+    {
+        if(!ok)
+        {
+            tolua_error(tolua_S,"invalid arguments in function 'lua_ax_base_Label_getTTFFaceSize'", nullptr);
+            return 0;
+        }
+        auto&& ret = cobj->getTTFFaceSize();
+        tolua_pushnumber(tolua_S,(lua_Number)ret);
+        return 1;
+    }
+    luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d \n", "ax.Label:getTTFFaceSize",argc, 0);
+    return 0;
+
+#if _AX_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_ax_base_Label_getTTFFaceSize'.",&tolua_err);
+#endif
+
+    return 0;
+}
 int lua_ax_base_Label_setBMFontSize(lua_State* tolua_S)
 {
     int argc = 0;
@@ -57398,6 +57495,8 @@ int lua_register_ax_base_Label(lua_State* tolua_S)
         tolua_function(tolua_S,"setLineBreakWithoutSpace",lua_ax_base_Label_setLineBreakWithoutSpace);
         tolua_function(tolua_S,"setMaxLineWidth",lua_ax_base_Label_setMaxLineWidth);
         tolua_function(tolua_S,"getMaxLineWidth",lua_ax_base_Label_getMaxLineWidth);
+        tolua_function(tolua_S,"setTTFFaceSize",lua_ax_base_Label_setTTFFaceSize);
+        tolua_function(tolua_S,"getTTFFaceSize",lua_ax_base_Label_getTTFFaceSize);
         tolua_function(tolua_S,"setBMFontSize",lua_ax_base_Label_setBMFontSize);
         tolua_function(tolua_S,"getBMFontSize",lua_ax_base_Label_getBMFontSize);
         tolua_function(tolua_S,"enableWrap",lua_ax_base_Label_enableWrap);
