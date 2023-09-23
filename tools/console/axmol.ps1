7ac1f271ff4e9cdd1da65ecab9359de644a457c4
halx99 <halx99@live.com>
2023-09-23 11:33:12 +0800

    Improve build scripts

--- tools/console/axmol.ps1
+++ tools/console/axmol.ps1
@@ -304,7 +304,7 @@ if (!$sub_args[0] -or $help) {
 
 $sub_opts = @{}
 if ($sub_args.IndexOf('-d') -eq -1) {
-    $sub_opts['-d'] = $(Get-Location).Path
+    $sub_opts['d'] = $(Get-Location).Path
 }
 
 . $plugin.proc @sub_args @sub_opts
