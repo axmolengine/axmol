ef7c19b51dd05cba5ee56a05cb70b0204629e426
Saroop Technologies <sarooptech@gmail.com>
2023-10-19 13:00:39 +0530

    Add JSONDefault extension (#1393)

---
+++ extensions/JSONDefault/JSONDefault.cpp
@@ -0,0 +1,302 @@
+/****************************************************************************
+Copyright (c) 2010-2012 cocos2d-x.org
+Copyright (c) 2013-2016 Chukong Technologies Inc.
+Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+Copyright (c) 2017-2020 C4games Ltd.
+Copyright (c) 2021-2022 Bytedance Inc.
+
+ https://axmolengine.github.io/
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+
+#include "JSONDefault.h"
+#include "base/Utils.h"
+#include "platform/Common.h"
+#include "platform/FileUtils.h"
+
+#include "rapidjson/prettywriter.h"
+#include "rapidjson/reader.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
+
+#define JSON_DEFAULT_FILENAME "JSONDefault.json"
+
+NS_AX_BEGIN
+
+/**
+ * implements of JSONDefault
+ */
+
+JSONDefault* JSONDefault::_jsonDefault = nullptr;
+
+JSONDefault::~JSONDefault() {}
+
+JSONDefault::JSONDefault()
+{
+    lazyInit();
+}
+
+JSONDefault* JSONDefault::getInstance()
+{
+    if (_jsonDefault)
+        return _jsonDefault;
+
+    return (_jsonDefault = new JSONDefault());
+}
+
+void JSONDefault::destroyInstance()
+{
+    AX_SAFE_DELETE(_jsonDefault);
+}
+
+void JSONDefault::setDelegate(JSONDefault* delegate)
+{
+    if (_jsonDefault)
+        delete _jsonDefault;
+
+    _jsonDefault = delegate;
+}
+
+void JSONDefault::lazyInit()
+{
+    if (_initialized)
+        return;
+
+    _doc.SetObject();
+
+    _filePath = FileUtils::getInstance()->getWritablePath() + JSON_DEFAULT_FILENAME;
+
+    if (FileUtils::getInstance()->isFileExist(_filePath))
+    {
+        auto data_str = FileUtils::getInstance()->getStringFromFile(_filePath);
+
+        _doc.Parse(data_str.c_str());
+
+        if (_doc.HasParseError())
+        {
+            log("JSONDefault::init load json file: %s failed", _filePath.c_str());
+            return;
+        }
+    }
+
+    _initialized = true;
+}
+
+// Getters
+bool JSONDefault::getBoolForKey(const char* key)
+{
+    return getBoolForKey(key, false);
+}
+
+bool JSONDefault::getBoolForKey(const char* key, bool defaultValue)
+{
+    if (_doc.HasMember(key))
+        return _doc[key].GetBool();
+    return defaultValue;
+}
+
+int JSONDefault::getIntegerForKey(const char* key)
+{
+    return getIntegerForKey(key, 0);
+}
+
+int JSONDefault::getIntegerForKey(const char* key, int defaultValue)
+{
+    if (_doc.HasMember(key))
+        return _doc[key].GetInt();
+    return defaultValue;
+}
+
+int64_t JSONDefault::getLargeIntForKey(const char* key)
+{
+    return getLargeIntForKey(key, 0);
+}
+
+int64_t JSONDefault::getLargeIntForKey(const char* key, int64_t defaultValue)
+{
+    if (_doc.HasMember(key))
+        return _doc[key].GetInt64();
+    return defaultValue;
+}
+
+float JSONDefault::getFloatForKey(const char* key)
+{
+    return getFloatForKey(key, 0.0f);
+}
+
+float JSONDefault::getFloatForKey(const char* key, float defaultValue)
+{
+    if (_doc.HasMember(key))
+        return _doc[key].GetFloat();
+    return defaultValue;
+}
+
+double JSONDefault::getDoubleForKey(const char* key)
+{
+    return getDoubleForKey(key, 0.0);
+}
+
+double JSONDefault::getDoubleForKey(const char* key, double defaultValue)
+{
+    if (_doc.HasMember(key))
+        return _doc[key].GetDouble();
+    return defaultValue;
+}
+
+std::string_view JSONDefault::getStringForKey(const char* key)
+{
+    return getStringForKey(key, "");
+}
+
+std::string_view JSONDefault::getStringForKey(const char* key, std::string_view defaultValue)
+{
+    if (_doc.HasMember(key))
+        return _doc[key].GetString();
+    return defaultValue;
+}
+
+// Setters
+void JSONDefault::setBoolForKey(const char* key, bool value)
+{
+    if (!key)
+        return;
+    if (_doc.HasMember(key))
+    {
+        _doc[key].SetBool(value);
+    }
+    else
+    {
+        _doc.AddMember(rapidjson::GenericStringRef(key), rapidjson::Value(value), _doc.GetAllocator());
+    }
+    flush();
+}
+
+void JSONDefault::setIntegerForKey(const char* key, int value)
+{
+    if (!key)
+        return;
+    if (_doc.HasMember(key))
+    {
+        _doc[key].SetInt(value);
+    }
+    else
+    {
+        _doc.AddMember(rapidjson::GenericStringRef(key), rapidjson::Value(value), _doc.GetAllocator());
+    }
+    flush();
+}
+
+void JSONDefault::setLargeIntForKey(const char* key, int64_t value)
+{  // check key
+    if (!key)
+        return;
+    if (_doc.HasMember(key))
+    {
+        _doc[key].SetInt64(value);
+    }
+    else
+    {
+        _doc.AddMember(rapidjson::GenericStringRef(key), rapidjson::Value(value), _doc.GetAllocator());
+    }
+    flush();
+}
+
+void JSONDefault::setFloatForKey(const char* key, float value)
+{
+    if (!key)
+        return;
+    if (_doc.HasMember(key))
+    {
+        _doc[key].SetFloat(value);
+    }
+    else
+    {
+        _doc.AddMember(rapidjson::GenericStringRef(key), rapidjson::Value(value), _doc.GetAllocator());
+    }
+    flush();
+}
+
+void JSONDefault::setDoubleForKey(const char* key, double value)
+{
+    if (!key)
+        return;
+    if (_doc.HasMember(key))
+    {
+        _doc[key].SetDouble(value);
+    }
+    else
+    {
+        _doc.AddMember(rapidjson::GenericStringRef(key), rapidjson::Value(value), _doc.GetAllocator());
+    }
+    flush();
+}
+
+void JSONDefault::setStringForKey(const char* key, const char* value)
+{
+    if (!key)
+        return;
+
+    if (_doc.HasMember(key))
+    {
+        _doc[key].SetString(rapidjson::GenericStringRef(value));
+    }
+    else
+    {
+        _doc.AddMember(rapidjson::GenericStringRef(key), rapidjson::GenericStringRef(value), _doc.GetAllocator());
+    }
+    flush();
+}
+
+// Delete
+void JSONDefault::deleteValueForKey(const char* key)
+{
+    if (!this->_doc.HasMember(key))
+        return;
+
+    this->_doc.RemoveMember(key);
+    flush();
+}
+
+// Save
+void JSONDefault::flush(bool pretty /*= false*/)
+{
+    rapidjson::StringBuffer buffer;
+
+    if (pretty)
+    {
+        rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
+        _doc.Accept(writer);
+    }
+    else
+    {
+        rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+        _doc.Accept(writer);
+    }
+    FileUtils::getInstance()->writeStringToFile(buffer.GetString(), _filePath);
+}
+
+// Reset
+void JSONDefault::reset()
+{
+    _doc.RemoveAllMembers();
+    flush();
+}
+
+NS_AX_END
---
+++ tests/cpp-tests/Source/ExtensionsTest/JSONDefaultTest/JSONDefaultTest.cpp
@@ -0,0 +1,137 @@
+/****************************************************************************
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+ Copyright (c) 2020 C4games Ltd.
+
+ https://axmolengine.github.io/
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+#include "JSONDefaultTest.h"
+#include "JSONDefault/JSONDefault.h"
+
+using namespace std;
+
+USING_NS_AX;
+
+JSONDefaultTests::JSONDefaultTests()
+{
+    ADD_TEST_CASE(JSONDefaultTest);
+}
+
+JSONDefaultTest::JSONDefaultTest()
+{
+    auto s     = Director::getInstance()->getWinSize();
+    auto label = Label::createWithTTF("CCJSONDefault test Log data see console", "fonts/arial.ttf", 22);
+    addChild(label, 0);
+    label->setPosition(Vec2(s.width / 2, s.height - 50));
+
+    this->_label = Label::createWithTTF("result", "fonts/arial.ttf", 12);
+    addChild(this->_label, 0);
+
+    label->setPosition(Vec2(s.width / 2, s.height - 50));
+    this->_label->setPosition(Vec2(s.width / 2, s.height / 2));
+
+    doTest();
+}
+
+void JSONDefaultTest::doTest()
+{
+    std::string strVal{this->_label->getString()};
+    this->_label->setString(
+        strVal.append("\n"
+                      "********************** init value ***********************"));
+
+    // set default value
+
+    JSONDefault::getInstance()->setStringForKey("string", "value1");
+    JSONDefault::getInstance()->setIntegerForKey("integer", 10);
+    JSONDefault::getInstance()->setFloatForKey("float", 2.3f);
+    JSONDefault::getInstance()->setDoubleForKey("double", 2.4);
+    JSONDefault::getInstance()->setBoolForKey("bool", true);
+
+    printValue();
+
+    strVal = this->_label->getString();
+    this->_label->setString(
+        strVal.append("\n"
+                      "********************** after change value ***********************"));
+
+    // change the value
+
+    JSONDefault::getInstance()->setStringForKey("string", "value2");
+    JSONDefault::getInstance()->setIntegerForKey("integer", 11);
+    JSONDefault::getInstance()->setFloatForKey("float", 2.5f);
+    JSONDefault::getInstance()->setDoubleForKey("double", 2.6);
+    JSONDefault::getInstance()->setBoolForKey("bool", false);
+
+    JSONDefault::getInstance()->flush();
+
+    // print value
+    printValue();
+
+    strVal = this->_label->getString();
+    this->_label->setString(
+        strVal.append("\n"
+                      "********************** after delete value ***********************"));
+
+    JSONDefault::getInstance()->deleteValueForKey("string");
+    JSONDefault::getInstance()->deleteValueForKey("integer");
+    JSONDefault::getInstance()->deleteValueForKey("float");
+    JSONDefault::getInstance()->deleteValueForKey("double");
+    JSONDefault::getInstance()->deleteValueForKey("bool");
+
+    // print value
+    printValue();
+}
+
+void JSONDefaultTest::printValue()
+{
+    char strTemp[256] = "";
+    // print value
+    std::string_view ret = JSONDefault::getInstance()->getStringForKey("string");
+    sprintf(strTemp, "string is %s", ret.data());
+    this->_label->setString((std::string)this->_label->getString() + "\n" + strTemp);
+
+    double d = JSONDefault::getInstance()->getDoubleForKey("double");
+    sprintf(strTemp, "double is %f", d);
+    this->_label->setString((std::string)this->_label->getString() + "\n" + strTemp);
+
+    int i = JSONDefault::getInstance()->getIntegerForKey("integer");
+    sprintf(strTemp, "integer is %d", i);
+    this->_label->setString((std::string)this->_label->getString() + "\n" + strTemp);
+
+    float f = JSONDefault::getInstance()->getFloatForKey("float");
+    sprintf(strTemp, "float is %f", f);
+    this->_label->setString((std::string)this->_label->getString() + "\n" + strTemp);
+
+    bool b = JSONDefault::getInstance()->getBoolForKey("bool");
+    if (b)
+    {
+        sprintf(strTemp, "bool is true");
+        this->_label->setString((std::string)this->_label->getString() + "\n" + strTemp);
+    }
+    else
+    {
+        sprintf(strTemp, "bool is false");
+        this->_label->setString((std::string)this->_label->getString() + "\n" + strTemp);
+    }
+}
+
+JSONDefaultTest::~JSONDefaultTest() {}
