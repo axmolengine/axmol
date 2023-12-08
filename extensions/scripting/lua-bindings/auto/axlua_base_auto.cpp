5d4db9d830fef10f886087042b167a47889beb5c
halx99 <halx99@users.noreply.github.com>
2023-12-08 05:23:29 +0000

    Committing genbindings changes

--- extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
+++ extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
@@ -50576,6 +50576,100 @@ int lua_register_ax_base_ActionTween(lua_State* tolua_S)
     return 1;
 }
 
+int lua_ax_base_ActionCoroutine_initWithCoroutine(lua_State* tolua_S)
+{
+    int argc = 0;
+    ax::ActionCoroutine* cobj = nullptr;
+    bool ok  = true;
+
+#if _AX_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if _AX_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"ax.ActionCoroutine",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    cobj = (ax::ActionCoroutine*)tolua_tousertype(tolua_S,1,0);
+
+#if _AX_DEBUG >= 1
+    if (!cobj) 
+    {
+        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_ax_base_ActionCoroutine_initWithCoroutine'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 1) 
+    {
+        std::function<ax::Coroutine ()> arg0;
+
+        do {
+			// Lambda binding for lua is not supported.
+			assert(false);
+		} while(0)
+		;
+        if(!ok)
+        {
+            tolua_error(tolua_S,"invalid arguments in function 'lua_ax_base_ActionCoroutine_initWithCoroutine'", nullptr);
+            return 0;
+        }
+        auto&& ret = cobj->initWithCoroutine(arg0);
+        tolua_pushboolean(tolua_S,(bool)ret);
+        return 1;
+    }
+    luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d \n", "ax.ActionCoroutine:initWithCoroutine",argc, 1);
+    return 0;
+
+#if _AX_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_ax_base_ActionCoroutine_initWithCoroutine'.",&tolua_err);
+#endif
+
+    return 0;
+}
+int lua_ax_base_ActionCoroutine_create(lua_State* tolua_S)
+{
+    int argc = 0;
+    bool ok  = true;
+
+#if _AX_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+#if _AX_DEBUG >= 1
+    if (!tolua_isusertable(tolua_S,1,"ax.ActionCoroutine",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    argc = lua_gettop(tolua_S) - 1;
+
+    if (argc == 1)
+    {
+        std::function<ax::Coroutine ()> arg0;
+        do {
+			// Lambda binding for lua is not supported.
+			assert(false);
+		} while(0)
+		;
+        if(!ok)
+        {
+            tolua_error(tolua_S,"invalid arguments in function 'lua_ax_base_ActionCoroutine_create'", nullptr);
+            return 0;
+        }
+        auto&& ret = ax::ActionCoroutine::create(arg0);
+        object_to_luaval<ax::ActionCoroutine>(tolua_S, "ax.ActionCoroutine",(ax::ActionCoroutine*)ret);
+        return 1;
+    }
+    luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d\n ", "ax.ActionCoroutine:create",argc, 1);
+    return 0;
+#if _AX_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_ax_base_ActionCoroutine_create'.",&tolua_err);
+#endif
+    return 0;
+}
 int lua_ax_base_ActionCoroutine_constructor(lua_State* tolua_S)
 {
     int argc = 0;
@@ -50626,6 +50720,8 @@ int lua_register_ax_base_ActionCoroutine(lua_State* tolua_S)
 
     tolua_beginmodule(tolua_S,"ActionCoroutine");
         tolua_function(tolua_S,"new",lua_ax_base_ActionCoroutine_constructor);
+        tolua_function(tolua_S,"initWithCoroutine",lua_ax_base_ActionCoroutine_initWithCoroutine);
+        tolua_function(tolua_S,"create", lua_ax_base_ActionCoroutine_create);
     tolua_endmodule(tolua_S);
     auto typeName = typeid(ax::ActionCoroutine).name(); // rtti is literal storage
     g_luaType[reinterpret_cast<uintptr_t>(typeName)] = "ax.ActionCoroutine";
