bedb11ac6baf67cf9079f4cf766b783f9f84816a
halx99 <halx99@live.com>
2024-02-22 00:21:21 +0800

    Update template cfg [skip ci][skip appveyor]

--- templates/cpp/Source/AppDelegate.cpp
+++ templates/cpp/Source/AppDelegate.cpp
@@ -63,9 +63,9 @@ bool AppDelegate::applicationDidFinishLaunching()
 #if (AX_TARGET_PLATFORM == AX_PLATFORM_WIN32) || (AX_TARGET_PLATFORM == AX_PLATFORM_MAC) || \
     (AX_TARGET_PLATFORM == AX_PLATFORM_LINUX)
         glView = GLViewImpl::createWithRect(
-            "HelloCpp", ax::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+            "Dummy", ax::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
 #else
-        glView = GLViewImpl::create("HelloCpp");
+        glView = GLViewImpl::create("Dummy");
 #endif
         director->setGLView(glView);
     }
