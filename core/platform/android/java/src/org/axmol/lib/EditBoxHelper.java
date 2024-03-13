cdf69d159a45d2b8eaac43bbdd81dee6d672afdc
RH <rh101@users.noreply.github.com>
2024-03-13 15:37:33 +1100

    Removing EditBox should not result in logging an error message (#1740)
    * Removing EditBox should not result in logging an error message
    
    * Add copyright notice

--- core/platform/android/java/src/org/axmol/lib/EditBoxHelper.java
+++ core/platform/android/java/src/org/axmol/lib/EditBoxHelper.java
@@ -2,6 +2,7 @@
 Copyright (c) 2010-2012 cocos2d-x.org
 Copyright (c) 2013-2016 Chukong Technologies Inc.
 Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+Copyright (c) 2019-present Axmol Engine contributors (see AUTHORS.md).
 
 https://axmolengine.github.io/
 
@@ -227,7 +228,7 @@ public class EditBoxHelper {
                 if (editBox != null) {
                     mEditBoxArray.remove(index);
                     mFrameLayout.removeView(editBox);
-                    Log.e(TAG, "remove EditBox");
+                    Log.d(TAG, "remove EditBox");
                 }
             }
         });
