8cacc925d1d4266650def41ba9ae900dfc600fd4
halx99 <6977319+halx99@users.noreply.github.com>
2024-03-15 13:30:05 +0000

    Committing luabindings for commit 45d302c

--- extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
+++ extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
@@ -10098,6 +10098,56 @@ int lua_ax_base_Node_getWorldPosition(lua_State* tolua_S)
 
     return 0;
 }
+int lua_ax_base_Node_setWorldPosition(lua_State* tolua_S)
+{
+    int argc = 0;
+    ax::Node* cobj = nullptr;
+    bool ok  = true;
+
+#if _AX_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if _AX_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"ax.Node",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    cobj = (ax::Node*)tolua_tousertype(tolua_S,1,0);
+
+#if _AX_DEBUG >= 1
+    if (!cobj) 
+    {
+        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_ax_base_Node_setWorldPosition'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 1) 
+    {
+        ax::Vec2 arg0;
+
+        ok &= luaval_to_vec2(tolua_S, 2, &arg0, "ax.Node:setWorldPosition");
+        if(!ok)
+        {
+            tolua_error(tolua_S,"invalid arguments in function 'lua_ax_base_Node_setWorldPosition'", nullptr);
+            return 0;
+        }
+        cobj->setWorldPosition(arg0);
+        lua_settop(tolua_S, 1);
+        return 1;
+    }
+    luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d \n", "ax.Node:setWorldPosition",argc, 1);
+    return 0;
+
+#if _AX_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_ax_base_Node_setWorldPosition'.",&tolua_err);
+#endif
+
+    return 0;
+}
 int lua_ax_base_Node_getComponent(lua_State* tolua_S)
 {
     int argc = 0;
@@ -12195,6 +12245,7 @@ int lua_register_ax_base_Node(lua_State* tolua_S)
         tolua_function(tolua_S,"convertTouchToNodeSpace",lua_ax_base_Node_convertTouchToNodeSpace);
         tolua_function(tolua_S,"convertTouchToNodeSpaceAR",lua_ax_base_Node_convertTouchToNodeSpaceAR);
         tolua_function(tolua_S,"getWorldPosition",lua_ax_base_Node_getWorldPosition);
+        tolua_function(tolua_S,"setWorldPosition",lua_ax_base_Node_setWorldPosition);
         tolua_function(tolua_S,"getComponent",lua_ax_base_Node_getComponent);
         tolua_function(tolua_S,"addComponent",lua_ax_base_Node_addComponent);
         tolua_function(tolua_S,"removeComponent",lua_ax_base_Node_removeComponent);
--- extensions/scripting/lua-bindings/auto/axlua_spine_auto.hpp
+++ extensions/scripting/lua-bindings/auto/axlua_spine_auto.hpp
@@ -4,38 +4,3 @@
 #include "tolua++.h"
 int register_all_ax_spine(lua_State* tolua_S);
 #endif // __ax_spine_h__
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#endif // __ax_spine_h__
