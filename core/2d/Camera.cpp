72f3d93556cd54b6830c4c7a299ed753020c02db
rh101 <rh101@users.noreply.github.com>
2024-03-28 09:43:56 +1100

    Add required copyright notice

--- core/2d/Camera.cpp
+++ core/2d/Camera.cpp
@@ -1,6 +1,7 @@
 /****************************************************************************
  Copyright (c) 2014-2016 Chukong Technologies Inc.
  Copyright (c) 2017-2019 Xiamen Yaji Software Co., Ltd.
+ Copyright (c) 2019-present Axmol Engine contributors (see AUTHORS.md).
 
  https://axmolengine.github.io/
 
