8109c77d7b51bdf6962f1d6fdb29dc8f614f464b
halx99 <6977319+halx99@users.noreply.github.com>
2024-03-28 02:51:42 +0000

    Committing luabindings for commit 4fb8452

--- extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
+++ extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
@@ -89741,56 +89741,6 @@ int lua_ax_base_Camera_projectGL(lua_State* tolua_S)
 
     return 0;
 }
-int lua_ax_base_Camera_isVisibleInFrustum(lua_State* tolua_S)
-{
-    int argc = 0;
-    ax::Camera* cobj = nullptr;
-    bool ok  = true;
-
-#if _AX_DEBUG >= 1
-    tolua_Error tolua_err;
-#endif
-
-
-#if _AX_DEBUG >= 1
-    if (!tolua_isusertype(tolua_S,1,"ax.Camera",0,&tolua_err)) goto tolua_lerror;
-#endif
-
-    cobj = (ax::Camera*)tolua_tousertype(tolua_S,1,0);
-
-#if _AX_DEBUG >= 1
-    if (!cobj) 
-    {
-        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_ax_base_Camera_isVisibleInFrustum'", nullptr);
-        return 0;
-    }
-#endif
-
-    argc = lua_gettop(tolua_S)-1;
-    if (argc == 1) 
-    {
-        const ax::AABB* arg0;
-
-        ok &= luaval_to_object<const ax::AABB>(tolua_S, 2, "ax.AABB",&arg0, "ax.Camera:isVisibleInFrustum");
-        if(!ok)
-        {
-            tolua_error(tolua_S,"invalid arguments in function 'lua_ax_base_Camera_isVisibleInFrustum'", nullptr);
-            return 0;
-        }
-        auto&& ret = cobj->isVisibleInFrustum(arg0);
-        tolua_pushboolean(tolua_S,(bool)ret);
-        return 1;
-    }
-    luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d \n", "ax.Camera:isVisibleInFrustum",argc, 1);
-    return 0;
-
-#if _AX_DEBUG >= 1
-    tolua_lerror:
-    tolua_error(tolua_S,"#ferror in function 'lua_ax_base_Camera_isVisibleInFrustum'.",&tolua_err);
-#endif
-
-    return 0;
-}
 int lua_ax_base_Camera_getDepthInView(lua_State* tolua_S)
 {
     int argc = 0;
@@ -91332,7 +91282,6 @@ int lua_register_ax_base_Camera(lua_State* tolua_S)
         tolua_function(tolua_S,"getViewProjectionMatrix",lua_ax_base_Camera_getViewProjectionMatrix);
         tolua_function(tolua_S,"project",lua_ax_base_Camera_project);
         tolua_function(tolua_S,"projectGL",lua_ax_base_Camera_projectGL);
-        tolua_function(tolua_S,"isVisibleInFrustum",lua_ax_base_Camera_isVisibleInFrustum);
         tolua_function(tolua_S,"getDepthInView",lua_ax_base_Camera_getDepthInView);
         tolua_function(tolua_S,"setDepth",lua_ax_base_Camera_setDepth);
         tolua_function(tolua_S,"getDepth",lua_ax_base_Camera_getDepth);
