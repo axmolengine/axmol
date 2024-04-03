620c5027fda4faed43e9f24edeb7308803d05eec
halx99 <halx99@live.com>
2024-04-03 20:25:25 +0800

    Update build system

--- 1k/fetch.cmake
+++ 1k/fetch.cmake
@@ -19,16 +19,21 @@ function(_1kfetch_init)
         set(_1kfetch_manifest "${_1kfetch_manifest}" CACHE STRING "" FORCE)
     endif()
 
-    execute_process(COMMAND ${PWSH_PROG} ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/fetchurl.ps1
+    execute_process(COMMAND ${PWSH_PROG} ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/resolv_uri.ps1
         -name "1kdist"
         -manifest ${_1kfetch_manifest}
         OUTPUT_VARIABLE _1kdist_url
     )
-    string(REPLACE "#" ";" _1kdist_url ${_1kdist_url})
-    list(GET _1kdist_url 0 _1kdist_base_url)
-    list(GET _1kdist_url 1 _1kdist_ver)
-    set(_1kdist_base_url "${_1kdist_base_url}/${_1kdist_ver}" PARENT_SCOPE)
-    set(_1kdist_ver ${_1kdist_ver} PARENT_SCOPE)
+
+    if(_1kdist_url)
+        string(REPLACE "#" ";" _1kdist_url ${_1kdist_url})
+        list(GET _1kdist_url 0 _1kdist_base_url)
+        list(GET _1kdist_url 1 _1kdist_ver)
+        set(_1kdist_base_url "${_1kdist_base_url}/${_1kdist_ver}" PARENT_SCOPE)
+        set(_1kdist_ver ${_1kdist_ver} PARENT_SCOPE)
+    else()
+        message(WARNING "Resolve 1kdist uri fail, the _1kfetch_dist will not work")
+    endif()
 endfunction()
 
 # fetch prebuilt from 1kdist
