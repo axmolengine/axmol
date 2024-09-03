a9b284506c4320977db26240ec5b762b6687eada
Julius Trinkunas <smilediver@outlook.com>
2024-09-03 20:34:57 +0300

    Mat4: remove inline file

--- core/math/Mat4.h
+++ core/math/Mat4.h
@@ -98,7 +98,7 @@ public:
      *     0  0  1  0
      *     0  0  0  1
      */
-    constexpr Mat4();
+    constexpr Mat4() { setIdentity(); }
 
     /**
      * Constructs a matrix initialized to the specified value.
@@ -135,7 +135,10 @@ public:
          float m41,
          float m42,
          float m43,
-         float m44);
+         float m44)
+    {
+        set(m11, m12, m13, m14, m21, m22, m23, m24, m31, m32, m33, m34, m41, m42, m43, m44);
+    }
 
     /**
      * Creates a matrix initialized to the specified column-major array.
@@ -149,14 +152,14 @@ public:
      *
      * @param mat An array containing 16 elements in column-major order.
      */
-    Mat4(const float* mat);
+    Mat4(const float* mat) { set(mat); }
 
     /**
      * Constructs a new matrix by copying the values from the specified matrix.
      *
      * @param copy The matrix to copy.
      */
-    Mat4(const Mat4& copy);
+    Mat4(const Mat4& copy) { memcpy(m, copy.m, sizeof(m)); }
 
     /**
      * Creates a view matrix based on the specified input parameters.
@@ -742,31 +745,56 @@ public:
              float m41,
              float m42,
              float m43,
-             float m44);
+             float m44)
+    {
+        m[0]  = m11;
+        m[1]  = m21;
+        m[2]  = m31;
+        m[3]  = m41;
+        m[4]  = m12;
+        m[5]  = m22;
+        m[6]  = m32;
+        m[7]  = m42;
+        m[8]  = m13;
+        m[9]  = m23;
+        m[10] = m33;
+        m[11] = m43;
+        m[12] = m14;
+        m[13] = m24;
+        m[14] = m34;
+        m[15] = m44;
+    }
 
     /**
      * Sets the values of this matrix to those in the specified column-major array.
      *
      * @param mat An array containing 16 elements in column-major format.
      */
-    void set(const float* mat);
+    void set(const float* mat)
+    {
+        GP_ASSERT(mat);
+        memcpy(m, mat, sizeof(m));
+    }
 
     /**
      * Sets the values of this matrix to those of the specified matrix.
      *
      * @param mat The source matrix.
      */
-    void set(const Mat4& mat);
+    void set(const Mat4& mat) { memcpy(m, mat.m, sizeof(m)); }
 
     /**
      * Sets this matrix to the identity matrix.
      */
-    constexpr void setIdentity();
+    constexpr void setIdentity()
+    {
+        set(1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+    }
 
     /**
      * Sets all elements of the current matrix to zero.
      */
-    void setZero();
+    void setZero() { memset(m, 0, sizeof(m)); }
 
     /**
      * Subtracts the specified matrix from the current matrix.
@@ -914,7 +942,12 @@ public:
      * @param mat The matrix to add.
      * @return The matrix sum.
      */
-    inline Mat4 operator+(const Mat4& mat) const;
+    Mat4 operator+(const Mat4& mat) const
+    {
+        Mat4 result(*this);
+        result.add(mat);
+        return result;
+    }
 
     /**
      * Adds the given matrix to this matrix.
@@ -922,7 +955,11 @@ public:
      * @param mat The matrix to add.
      * @return This matrix, after the addition occurs.
      */
-    inline Mat4& operator+=(const Mat4& mat);
+    Mat4& operator+=(const Mat4& mat)
+    {
+        add(mat);
+        return *this;
+    }
 
     /**
      * Calculates the difference of this matrix with the given matrix.
@@ -932,7 +969,12 @@ public:
      * @param mat The matrix to subtract.
      * @return The matrix difference.
      */
-    inline Mat4 operator-(const Mat4& mat) const;
+    Mat4 operator-(const Mat4& mat) const
+    {
+        Mat4 result(*this);
+        result.subtract(mat);
+        return result;
+    }
 
     /**
      * Subtracts the given matrix from this matrix.
@@ -940,7 +982,11 @@ public:
      * @param mat The matrix to subtract.
      * @return This matrix, after the subtraction occurs.
      */
-    inline Mat4& operator-=(const Mat4& mat);
+    Mat4& operator-=(const Mat4& mat)
+    {
+        subtract(mat);
+        return *this;
+    }
 
     /**
      * Calculates the negation of this matrix.
@@ -949,7 +995,12 @@ public:
      *
      * @return The negation of this matrix.
      */
-    inline Mat4 operator-() const;
+    Mat4 operator-() const
+    {
+        Mat4 mat(*this);
+        mat.negate();
+        return mat;
+    }
 
     /**
      * Calculates the matrix product of this matrix with the given matrix.
@@ -959,7 +1010,12 @@ public:
      * @param mat The matrix to multiply by.
      * @return The matrix product.
      */
-    inline Mat4 operator*(const Mat4& mat) const;
+    Mat4 operator*(const Mat4& mat) const
+    {
+        Mat4 result(*this);
+        result.multiply(mat);
+        return result;
+    }
 
     /**
      * Right-multiplies this matrix by the given matrix.
@@ -967,7 +1023,11 @@ public:
      * @param mat The matrix to multiply by.
      * @return This matrix, after the multiplication occurs.
      */
-    inline Mat4& operator*=(const Mat4& mat);
+    Mat4& operator*=(const Mat4& mat)
+    {
+        multiply(mat);
+        return *this;
+    }
 
     /** equals to a matrix full of zeros */
     static const Mat4 ZERO;
@@ -991,7 +1051,11 @@ private:
  * @param m The matrix to transform by.
  * @return This vector, after the transformation occurs.
  */
-inline Vec3& operator*=(Vec3& v, const Mat4& m);
+inline Vec3& operator*=(Vec3& v, const Mat4& m)
+{
+    m.transformVector(&v);
+    return v;
+}
 
 /**
  * Transforms the given vector by the given matrix.
@@ -1002,7 +1066,12 @@ inline Vec3& operator*=(Vec3& v, const Mat4& m);
  * @param v The vector to transform.
  * @return The resulting transformed vector.
  */
-inline Vec3 operator*(const Mat4& m, const Vec3& v);
+inline Vec3 operator*(const Mat4& m, const Vec3& v)
+{
+    Vec3 x;
+    m.transformVector(v, &x);
+    return x;
+}
 
 /**
  * Transforms the given vector by the given matrix.
@@ -1013,7 +1082,11 @@ inline Vec3 operator*(const Mat4& m, const Vec3& v);
  * @param m The matrix to transform by.
  * @return This vector, after the transformation occurs.
  */
-inline Vec4& operator*=(Vec4& v, const Mat4& m);
+inline Vec4& operator*=(Vec4& v, const Mat4& m)
+{
+    m.transformVector(&v);
+    return v;
+}
 
 /**
  * Transforms the given vector by the given matrix.
@@ -1024,13 +1097,22 @@ inline Vec4& operator*=(Vec4& v, const Mat4& m);
  * @param v The vector to transform.
  * @return The resulting transformed vector.
  */
-inline Vec4 operator*(const Mat4& m, const Vec4& v);
+inline Vec4 operator*(const Mat4& m, const Vec4& v)
+{
+    Vec4 x;
+    m.transformVector(v, &x);
+    return x;
+}
+
+#if !(defined(AX_DLLEXPORT) || defined(AX_DLLIMPORT))
+    inline constexpr Mat4 Mat4::IDENTITY(1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+    inline constexpr Mat4 Mat4::ZERO(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+#endif
 
 NS_AX_MATH_END
 /**
  end of base group
  @}
  */
-#include "math/Mat4.inl"
 
 #endif  // MATH_MAT4_H
