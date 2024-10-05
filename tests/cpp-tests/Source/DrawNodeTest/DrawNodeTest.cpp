de1e77a1f398e588e5e1831d4b01ce04f00fa64f
halx99 <halx99@live.com>
2024-10-05 20:55:21 +0800

    Make DrawNodeTest CandyMixEffect behavior same with 2.x

--- tests/cpp-tests/Source/DrawNodeTest/DrawNodeTest.cpp
+++ tests/cpp-tests/Source/DrawNodeTest/DrawNodeTest.cpp
@@ -3744,10 +3744,11 @@ void CandyMixEeffect::update(float dt)
 {
     DrawNodeBaseTest::update(dt);
     drawNode->clear();
+    static b2Timer timer = b2CreateTimer();
 
     static float WID = 400;
     static float HIG = 600;
-    float t  = utils::getTimeInMilliseconds() / 1000.0f;
+    float t          = b2GetMilliseconds(&timer) / 1000.0f;
     float ta = sin(t * cos(t) * 0.02f) + t;
     float tb = (1.0f + sin(t) * 1.0f) * 0.02f + 0.01f;
     float xa = WID * 0.5f;
