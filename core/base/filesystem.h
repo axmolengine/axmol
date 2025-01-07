6cd5aab668811432f6b921fabf971139e0a129d8
halx99 <halx99@live.com>
2025-01-08 00:39:24 +0800

    Update Android & iOS minmal OS support
    Android: Android 5.1(API 21)+
    iOS: 13+

--- core/base/filesystem.h
+++ core/base/filesystem.h
@@ -1,12 +1,5 @@
-// The stdfs workaround header, if not avaiable on current system, use ghc::filesystem
+// The filesystem works on ndk r23+ and ios 13+
 #pragma once
 
-/* ios < 13 or ndk < 22, ghc as workaround */
-#if (defined(__IPHONE_OS_VERSION_MIN_REQUIRED) && __IPHONE_OS_VERSION_MIN_REQUIRED < 130000) || \
-    (defined(__NDK_MAJOR__) && __NDK_MAJOR__ < 22)
-#    include "ghc/filesystem.hpp"
-namespace stdfs = ghc::filesystem;
-#else
-#    include <filesystem>
+#include <filesystem>
 namespace stdfs = std::filesystem;
-#endif
