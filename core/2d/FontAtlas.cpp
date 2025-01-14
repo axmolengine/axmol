bcbe8e9e3c9d09627006d26bb2a70d4f57b76f5f
halx99 <halx99@live.com>
2025-01-15 02:17:39 +0800

    Improve code style

--- core/2d/FontAtlas.cpp
+++ core/2d/FontAtlas.cpp
@@ -42,11 +42,10 @@
 #include "zlib.h"
 #include "base/ZipUtils.h"
 
-#include "base/PaddedString.h"
+#include "base/json.h"
 
 namespace ax
 {
-
 const int FontAtlas::CacheTextureWidth     = 512;
 const int FontAtlas::CacheTextureHeight    = 512;
 const char* FontAtlas::CMD_PURGE_FONTATLAS = "__ax_PURGE_FONTATLAS";
@@ -54,11 +53,12 @@ const char* FontAtlas::CMD_RESET_FONTATLAS = "__ax_RESET_FONTATLAS";
 
 void FontAtlas::loadFontAtlas(std::string_view fontatlasFile, hlookup::string_map<FontAtlas*>& outAtlasMap)
 {
-    using namespace simdjson;
+    using namespace ::simdjson;
 
     try
     {
-        auto strJson = PaddedString::load(fontatlasFile);
+        simdjson::PaddedString strJson;
+        FileUtils::getInstance()->getContents(fontatlasFile, &strJson);
         ondemand::parser parser;
         ondemand::document settings = parser.iterate(strJson);
         std::string_view type       = settings["type"];
