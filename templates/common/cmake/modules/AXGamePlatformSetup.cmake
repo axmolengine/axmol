40e696134e0143ad984a5642e83ef71ba9727765
rh101 <rh101@users.noreply.github.com>
2025-03-03 13:56:55 +1100

    More platform setup sections into one file

--- templates/common/cmake/modules/AXGamePlatformSetup.cmake
+++ templates/common/cmake/modules/AXGamePlatformSetup.cmake
@@ -16,4 +16,14 @@ if(APPLE)
 
     # For code-signing, set the DEVELOPMENT_TEAM:
     #set_xcode_property(${APP_NAME} DEVELOPMENT_TEAM "GRLXXXX2K9")
+elseif(WINDOWS)
+    # config quick starter batch script run.bat for windows
+    if(WIN32)
+        file(RELATIVE_PATH CMAKE_BUILD_RELATIVE_DIR "${CMAKE_CURRENT_SOURCE_DIR}" "${PROJECT_BINARY_DIR}")
+        configure_file("${CMAKE_CURRENT_SOURCE_DIR}/run.bat.in" "${CMAKE_CURRENT_SOURCE_DIR}/run.bat" @ONLY)
+    endif()
+    
+    if(NOT _AX_USE_PREBUILT)
+        ax_sync_target_dlls(${APP_NAME})
+    endif()
 endif()
