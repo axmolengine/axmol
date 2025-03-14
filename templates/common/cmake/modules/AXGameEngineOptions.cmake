0b3e13d2ef78b5d1751ac821fbe89d58153a3d16
rh101 <rh101@users.noreply.github.com>
2025-03-15 10:44:14 +1100

    Make the engine options more accessible

---
+++ templates/common/cmake/modules/AXGameEngineOptions.cmake
@@ -0,0 +1,31 @@
+#set(AX_EXT_HINT OFF CACHE BOOL "The default extensions hint" FORCE)
+
+# The following are by default set the same as the AX_EXT_HINT flag above
+# Uncomment the lines below to set them individually
+#set(AX_ENABLE_EXT_LUA OFF CACHE BOOL "Build lua libraries" FORCE)
+#set(AX_ENABLE_EXT_GUI OFF CACHE BOOL "Build extension GUI" FORCE)
+#set(AX_ENABLE_EXT_ASSETMANAGER OFF CACHE BOOL "Build extension asset-manager" FORCE)
+#set(AX_ENABLE_EXT_SPINE OFF CACHE BOOL "Build extension spine" FORCE)
+#set(AX_ENABLE_EXT_DRAGONBONES OFF CACHE BOOL "Build extension DragonBones" FORCE)
+#set(AX_ENABLE_EXT_COCOSTUDIO OFF CACHE BOOL "Build extension cocostudio" FORCE)
+#set(AX_ENABLE_EXT_FAIRYGUI OFF CACHE BOOL "Build extension FairyGUI" FORCE)
+
+# The follow options are set individually
+#set(AX_ENABLE_EXT_LIVE2D OFF CACHE BOOL "Build extension Live2D" FORCE)
+#set(AX_ENABLE_EXT_EFFEKSEER OFF CACHE BOOL "Build extension Effekseer" FORCE)
+#set(AX_ENABLE_EXT_IMGUI OFF CACHE BOOL "Build extension ImGui" FORCE)
+#set(AX_ENABLE_EXT_INSPECTOR OFF CACHE BOOL "Enable extension Inspector" FORCE)
+
+#set(AX_ENABLE_3D OFF CACHE BOOL "Build 3D support" FORCE)
+#set(AX_ENABLE_EXT_PARTICLE3D OFF CACHE BOOL "Build extension Particle3D" FORCE)
+#set(AX_ENABLE_3D_PHYSICS OFF CACHE BOOL "Build Physics3D support" FORCE)
+#set(AX_ENABLE_NAVMESH OFF CACHE BOOL "Build NavMesh support" FORCE)
+
+#set(AX_ENABLE_EXT_JSONDEFAULT ON CACHE BOOL "Build extension JSONDefault" FORCE)
+#set(AX_ENABLE_EXT_PHYSICS_NODE ON CACHE BOOL "Build extension physics-nodes" FORCE)
+
+#set(AX_ENABLE_PHYSICS ON CACHE BOOL "Build Physics3D support" FORCE)
+#set(AX_ENABLE_AUDIO ON CACHE BOOL "Build audio support" FORCE)
+
+#set(AX_ENABLE_WEBSOCKET ON CACHE BOOL "Build Websocket client based on yasio" FORCE)
+#set(AX_ENABLE_HTTP ON CACHE BOOL "Build HTTP client based on yasio" FORCE)
