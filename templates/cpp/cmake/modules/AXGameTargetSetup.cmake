ed2021b12f53809ad87bad1e81b50c980c03de3a
IamSanjid <iamsanjidhaque@gmail.com>
2025-03-17 21:51:31 +0600

    Fix BUILD_SHARED_LIBS for Android

--- templates/cpp/cmake/modules/AXGameTargetSetup.cmake
+++ templates/cpp/cmake/modules/AXGameTargetSetup.cmake
@@ -2,8 +2,12 @@ if(NOT ANDROID)
     add_executable(${APP_NAME} ${APP_SOURCES})
 else()
     add_library(${APP_NAME} SHARED ${APP_SOURCES})
-    # whole archive for jni
-    target_link_libraries(${APP_NAME} -Wl,--whole-archive cpp_android_spec -Wl,--no-whole-archive)
+
+    # whole archive for jni when not building engine as shared libs, otherwise libaxmol.so is archived with it.
+    if (NOT BUILD_SHARED_LIBS)
+        target_link_libraries(${APP_NAME} -Wl,--whole-archive cpp_android_spec -Wl,--no-whole-archive)
+    endif()
+
     config_android_shared_libs("dev.axmol.lib" "${CMAKE_CURRENT_SOURCE_DIR}/proj.android/app/src")
 endif()
 
