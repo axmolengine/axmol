813ef6dc8ce9b66fcddf21f89effd808b0c4000e
rh101 <rh101@users.noreply.github.com>
2025-05-28 11:56:10 +1000

    Update defaults

--- templates/common/cmake/modules/AXGameEngineOptions.cmake
+++ templates/common/cmake/modules/AXGameEngineOptions.cmake
@@ -1,6 +1,6 @@
-#set(AX_EXT_HINT OFF CACHE BOOL "The default extensions hint" FORCE)
+set(AX_EXT_HINT ON CACHE BOOL "The default extensions hint" FORCE)
 
-# The following are by default set the same as the AX_EXT_HINT flag above
+# The following are by default set the same as the AX_EXT_HINT flag above - default: ON
 # Uncomment the lines below to set them individually
 #set(AX_ENABLE_EXT_LUA OFF CACHE BOOL "Build lua libraries" FORCE)
 #set(AX_ENABLE_EXT_GUI OFF CACHE BOOL "Build extension GUI" FORCE)
@@ -9,23 +9,30 @@
 #set(AX_ENABLE_EXT_DRAGONBONES OFF CACHE BOOL "Build extension DragonBones" FORCE)
 #set(AX_ENABLE_EXT_COCOSTUDIO OFF CACHE BOOL "Build extension cocostudio" FORCE)
 #set(AX_ENABLE_EXT_FAIRYGUI OFF CACHE BOOL "Build extension FairyGUI" FORCE)
-
-# The follow options are set individually
-#set(AX_ENABLE_EXT_LIVE2D OFF CACHE BOOL "Build extension Live2D" FORCE)
-#set(AX_ENABLE_EXT_EFFEKSEER OFF CACHE BOOL "Build extension Effekseer" FORCE)
 #set(AX_ENABLE_EXT_IMGUI OFF CACHE BOOL "Build extension ImGui" FORCE)
-#set(AX_ENABLE_EXT_INSPECTOR OFF CACHE BOOL "Enable extension Inspector" FORCE)
+#set(AX_ENABLE_EXT_JSONDEFAULT OFF CACHE BOOL "Build extension JSONDefault" FORCE)
 
-#set(AX_ENABLE_3D OFF CACHE BOOL "Build 3D support" FORCE)
-#set(AX_ENABLE_EXT_PARTICLE3D OFF CACHE BOOL "Build extension Particle3D" FORCE)
-#set(AX_ENABLE_3D_PHYSICS OFF CACHE BOOL "Build Physics3D support" FORCE)
-#set(AX_ENABLE_NAVMESH OFF CACHE BOOL "Build NavMesh support" FORCE)
+# 3D Support - - default: ON
+set(AX_ENABLE_3D ON CACHE BOOL "Build 3D support" FORCE)
+# These depend on AX_EXT_HINT & AX_ENABLE_3D
+#set(AX_ENABLE_EXT_PARTICLE3D ON CACHE BOOL "Build extension Particle3D" FORCE)
+#set(AX_ENABLE_3D_PHYSICS ON CACHE BOOL "Build Physics3D support" FORCE)
+#set(AX_ENABLE_NAVMESH ON CACHE BOOL "Build NavMesh support" FORCE)
 
-#set(AX_ENABLE_EXT_JSONDEFAULT ON CACHE BOOL "Build extension JSONDefault" FORCE)
+# Physics Support - - default: ON
+set(AX_ENABLE_PHYSICS ON CACHE BOOL "Build Physics support" FORCE)
+# These depend on AX_EXT_HINT & AX_ENABLE_PHYSICS
 #set(AX_ENABLE_EXT_PHYSICS_NODE ON CACHE BOOL "Build extension physics-nodes" FORCE)
 
-#set(AX_ENABLE_PHYSICS ON CACHE BOOL "Build Physics3D support" FORCE)
-#set(AX_ENABLE_AUDIO ON CACHE BOOL "Build audio support" FORCE)
+# These depend on AX_EXT_HINT & AX_ENABLE_EXT_IMGUI - default: ON
+#set(AX_ENABLE_EXT_INSPECTOR ON CACHE BOOL "Enable extension Inspector" FORCE)
+#set(AX_ENABLE_EXT_SDFGEN ON CACHE BOOL "Build extension SDFGen" FORCE)
 
-#set(AX_ENABLE_WEBSOCKET ON CACHE BOOL "Build Websocket client based on yasio" FORCE)
-#set(AX_ENABLE_HTTP ON CACHE BOOL "Build HTTP client based on yasio" FORCE)
+# The follow options are set individually - default: OFF
+#set(AX_ENABLE_EXT_LIVE2D OFF CACHE BOOL "Build extension Live2D" FORCE)
+#set(AX_ENABLE_EXT_EFFEKSEER OFF CACHE BOOL "Build extension Effekseer" FORCE)
+
+# Code modules that can be disabled - default: ON
+set(AX_ENABLE_AUDIO ON CACHE BOOL "Build audio support" FORCE)
+set(AX_ENABLE_WEBSOCKET ON CACHE BOOL "Build Websocket client based on yasio" FORCE)
+set(AX_ENABLE_HTTP ON CACHE BOOL "Build HTTP client based on yasio" FORCE)
