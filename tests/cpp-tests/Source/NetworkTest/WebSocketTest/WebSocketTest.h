3245daa6eef8bc1449f45a29c3cfb5a7c33aeb40
AlexandreK38 <743370+AlexandreK38@users.noreply.github.com>
2025-06-27 00:41:24 +0200

    missing override in test header

--- tests/cpp-tests/Source/NetworkTest/WebSocketTest/WebSocketTest.h
+++ tests/cpp-tests/Source/NetworkTest/WebSocketTest/WebSocketTest.h
@@ -87,7 +87,7 @@ public:
 
     virtual void onOpen(ax::network::WebSocket* ws)override;
     virtual void onMessage(ax::network::WebSocket* ws, const ax::network::WebSocket::Data& data)override;
-    virtual void onClose(ax::network::WebSocket* ws)override;
+    virtual void onClose(ax::network::WebSocket* ws, uint16_t code, std::string_view reason)override;
     virtual void onError(ax::network::WebSocket* ws, const ax::network::WebSocket::ErrorCode& error)override;
 
     WebSocketCloseTest();
