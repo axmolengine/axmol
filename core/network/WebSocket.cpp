58408681c12ce98fbf3e40fd728e68884f51c450
AlexandreK38 <743370+AlexandreK38@users.noreply.github.com>
2025-06-27 15:43:22 +0200

    constructor

--- core/network/WebSocket.cpp
+++ core/network/WebSocket.cpp
@@ -451,7 +451,7 @@ void WebSocket::close(uint16_t code, std::string_view reason)
     if (_state < State::CLOSING)
     {
         _closeCode   = code;
-        _closeReason = std::string(reason.data(), reason.size());
+        _closeReason = std::string(reason);
         
         if (_service->is_open(0))
         {
