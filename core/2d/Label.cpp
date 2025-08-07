7bf5f87ef278a117132c363f985df93f2eeaf152
Thang Nguyen <125545428+NgVThangBz@users.noreply.github.com>
2025-08-07 12:04:51 +0700

    fix Label render

--- core/2d/Label.cpp
+++ core/2d/Label.cpp
@@ -1293,7 +1293,7 @@ bool Label::updateTTFConfigInternal()
         reset();
         return false;
     }
-
+    _contentDirty     = true;
     _currentLabelType = LabelType::TTF;
     setFontAtlas(newAtlas, _fontConfig.distanceFieldEnabled, true);
 
@@ -1305,7 +1305,9 @@ bool Label::updateTTFConfigInternal()
     }
     else
     {
-        _currLabelEffect = LabelEffect::NORMAL;
+        if(_currLabelEffect != LabelEffect::GLOW){
+            _currLabelEffect = LabelEffect::NORMAL;
+        }
         updateShaderProgram();
     }
 
@@ -1370,14 +1372,14 @@ void Label::enableGlow(const Color4B& glowColor)
 {
     if (_currentLabelType == LabelType::TTF)
     {
+        auto config                 = _fontConfig;
+        config.outlineSize          = 0;
         if (_fontConfig.distanceFieldEnabled == false)
         {
-            auto config                 = _fontConfig;
-            config.outlineSize          = 0;
             config.distanceFieldEnabled = true;
-            setTTFConfig(config);
-            _contentDirty = true;
         }
+        setTTFConfig(config);
+        _contentDirty = true;
         _currLabelEffect = LabelEffect::GLOW;
         _effectColorF.r  = glowColor.r / 255.0f;
         _effectColorF.g  = glowColor.g / 255.0f;
@@ -1401,18 +1403,15 @@ void Label::enableOutline(const Color4B& outlineColor, int outlineSize /* = -1 *
             _effectColorF.b = outlineColor.b / 255.0f;
             _effectColorF.a = outlineColor.a / 255.0f;
 
-            if (!_useDistanceField)
-            {  // not SDF, request font atlas from feetype
-                if (outlineSize > 0 && _fontConfig.outlineSize != outlineSize)
-                {
-                    _fontConfig.outlineSize = outlineSize;
-                    setTTFConfig(_fontConfig);
-                }
-            }
-            else
+            if (outlineSize > 0 && _fontConfig.outlineSize != outlineSize)
             {
-                if (outlineSize > 0)
-                    _currLabelEffect = LabelEffect::OUTLINE;
+                
+                _fontConfig.outlineSize = outlineSize;
+                setTTFConfig(_fontConfig);
+            }
+            if (_useDistanceField && outlineSize > 0)
+            {
+                _currLabelEffect = LabelEffect::OUTLINE;
                 updateShaderProgram();
             }
         }
@@ -1578,7 +1577,7 @@ void Label::disableEffect(LabelEffect effect)
         if (_boldEnabled)
         {
             _boldEnabled = false;
-            _additionalKerning -= 1;
+            setAdditionalKerning(_additionalKerning - 1);
             disableEffect(LabelEffect::SHADOW);
         }
         break;
