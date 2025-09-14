2c446304da55d1d37ae3c0f5e1d38e48e257b44a
halx99 <6977319+halx99@users.noreply.github.com>
2025-09-14 12:29:52 +0000

    Committing luabindings for commit ac8e144

--- extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
+++ extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
@@ -3725,6 +3725,53 @@ int lua_register_ax_base_EventTouch(lua_State* tolua_S)
     return 1;
 }
 
+int lua_ax_base_EventKeyboard_isRepeat(lua_State* tolua_S)
+{
+    int argc = 0;
+    ax::EventKeyboard* cobj = nullptr;
+    bool ok  = true;
+
+#if _AX_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if _AX_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"ax.EventKeyboard",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    cobj = (ax::EventKeyboard*)tolua_tousertype(tolua_S,1,0);
+
+#if _AX_DEBUG >= 1
+    if (!cobj)
+    {
+        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_ax_base_EventKeyboard_isRepeat'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 0) 
+    {
+        if(!ok)
+        {
+            tolua_error(tolua_S,"invalid arguments in function 'lua_ax_base_EventKeyboard_isRepeat'", nullptr);
+            return 0;
+        }
+        auto&& ret = cobj->isRepeat();
+        tolua_pushboolean(tolua_S,(bool)ret);
+        return 1;
+    }
+    luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d \n", "ax.EventKeyboard:isRepeat",argc, 0);
+    return 0;
+
+#if _AX_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_ax_base_EventKeyboard_isRepeat'.",&tolua_err);
+#endif
+
+    return 0;
+}
 int lua_ax_base_EventKeyboard_constructor(lua_State* tolua_S)
 {
     int argc = 0;
@@ -3758,6 +3805,29 @@ int lua_ax_base_EventKeyboard_constructor(lua_State* tolua_S)
         toluafix_pushusertype_object(tolua_S, ID, luaID, (void*)cobj,"ax.EventKeyboard");
         return 1;
     }
+    if (argc == 3) 
+    {
+        ax::EventKeyboard::KeyCode arg0;
+        bool arg1;
+        bool arg2;
+
+        ok &= luaval_to_int32(tolua_S, 2,(int *)&arg0, "ax.EventKeyboard:EventKeyboard");
+
+        ok &= luaval_to_boolean(tolua_S, 3,&arg1, "ax.EventKeyboard:EventKeyboard");
+
+        ok &= luaval_to_boolean(tolua_S, 4,&arg2, "ax.EventKeyboard:EventKeyboard");
+        if(!ok)
+        {
+            tolua_error(tolua_S,"invalid arguments in function 'lua_ax_base_EventKeyboard_constructor'", nullptr);
+            return 0;
+        }
+        cobj = new ax::EventKeyboard(arg0, arg1, arg2);
+        cobj->autorelease();
+        int ID =  (int)cobj->_ID ;
+        int* luaID =  &cobj->_luaID ;
+        toluafix_pushusertype_object(tolua_S, ID, luaID, (void*)cobj,"ax.EventKeyboard");
+        return 1;
+    }
     luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d \n", "ax.EventKeyboard:EventKeyboard",argc, 2);
     return 0;
 
@@ -3781,6 +3851,7 @@ int lua_register_ax_base_EventKeyboard(lua_State* tolua_S)
 
     tolua_beginmodule(tolua_S,"EventKeyboard");
         tolua_function(tolua_S,"new",lua_ax_base_EventKeyboard_constructor);
+        tolua_function(tolua_S,"isRepeat",lua_ax_base_EventKeyboard_isRepeat);
     tolua_endmodule(tolua_S);
     auto typeName = typeid(ax::EventKeyboard).name(); // rtti is literal storage
     g_luaType[reinterpret_cast<uintptr_t>(typeName)] = "ax.EventKeyboard";
