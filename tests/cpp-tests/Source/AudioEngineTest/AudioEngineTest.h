30c91541ea0e79134cb5ca6f63628646185c9135
rh101 <rh101@users.noreply.github.com>
2025-09-19 01:24:20 +1000

    Add reverb test

--- tests/cpp-tests/Source/AudioEngineTest/AudioEngineTest.h
+++ tests/cpp-tests/Source/AudioEngineTest/AudioEngineTest.h
@@ -384,4 +384,32 @@ private:
     ax::Label* _playOverLabel;
 };
 
+class AudioReverbTest : public AudioEngineTestDemo
+{
+public:
+    CREATE_FUNC(AudioReverbTest);
+
+    ~AudioReverbTest() override;
+
+    bool init() override;
+    void update(float dt) override;
+    std::string title() const override;
+
+private:
+    int _audioID;
+    bool _loopEnabled;
+    float _volume;
+    float _duration;
+    float _timeRatio;
+
+    void* _playItem;
+    void* _timeSlider;
+    bool _updateTimeSlider;
+    bool _isStopped;
+    ax::Label* _playOverLabel;
+    std::map<std::string, ax::EaxReverbSettings> _reverbSettingsMap;
+    std::map<std::string, ax::EaxReverbSettings>::const_iterator _currentReverbItr;
+};
+
+
 #endif /* defined(__NEWAUDIOENGINE_TEST_H_) */
