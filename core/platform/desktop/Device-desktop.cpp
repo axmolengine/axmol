082c7ad1520e9a2b76382f72a3d4bba8bdc728c0
halx99 <halx99@live.com>
2025-09-21 00:45:11 +0800

    fixup

--- core/platform/desktop/Device-desktop.cpp
+++ core/platform/desktop/Device-desktop.cpp
@@ -22,7 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/ \
-#include "axmol/platform/Device.h"
+#include "platform/Device.h"
 #include "GLFW/glfw3.h"
 
 namespace ax
