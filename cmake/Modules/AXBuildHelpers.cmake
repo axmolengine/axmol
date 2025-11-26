8e693bd4806e2b3474a3f8d0b751f75a4b30dace
halx99 <halx99@live.com>
2025-11-26 15:07:41 +0800

    d3d12: embed dxcompiler.dll into target bin output dir

--- cmake/Modules/AXBuildHelpers.cmake
+++ cmake/Modules/AXBuildHelpers.cmake
@@ -214,6 +214,9 @@ function(ax_sync_target_dlls ax_target)
   if(AX_GLES_PROFILE OR AX_RENDER_API MATCHES "d3d")
     find_windows_sdk_bin(_winsdk_bin_dir ${ARCH_ALIAS})
     list(APPEND all_depend_dlls "${_winsdk_bin_dir}/d3dcompiler_47.dll")
+    if(AX_RENDER_API STREQUAL "d3d12")
+      list(APPEND all_depend_dlls "${_winsdk_bin_dir}/dxcompiler.dll")
+    endif()
   endif()
 
   # Copy webview2 for ninja
