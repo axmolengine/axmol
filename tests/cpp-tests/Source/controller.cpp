c41f6f43f575d9c0f63d2d3d52753f6a81962331
halx99 <halx99@live.com>
2025-11-27 13:45:27 +0800

    fixup

--- tests/cpp-tests/Source/controller.cpp
+++ tests/cpp-tests/Source/controller.cpp
@@ -58,7 +58,7 @@ public:
         addTest("AudioEngine", []() { return new AudioEngineTests(); });
 
         addTest("Box2D - Basic", []() { return new Box2DTests(); });
-#if AX_ENABLE_EXT_IMGUI
+#if AX_ENABLE_EXT_IMGUI && AX_TARGET_PLATFORM != AX_PLATFORM_ANDROID
         addTest("Box2D - TestBed", []() { return new Box2DTestBedTests(); });
 #endif
         addTest("Chipmunk2D - Basic", []() { return new ChipmunkTests(); });
