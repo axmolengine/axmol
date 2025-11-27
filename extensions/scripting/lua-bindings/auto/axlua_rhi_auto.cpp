f5660e71b4952ea854e2cc089608a2053f43e589
halx99 <6977319+halx99@users.noreply.github.com>
2025-11-27 15:01:11 +0000

    Committing luabindings for commit ffe6144

--- extensions/scripting/lua-bindings/auto/axlua_rhi_auto.cpp
+++ extensions/scripting/lua-bindings/auto/axlua_rhi_auto.cpp
@@ -3003,6 +3003,53 @@ int lua_ax_rhi_DriverBase_cleanPendingResources(lua_State* tolua_S)
 
     return 0;
 }
+int lua_ax_rhi_DriverBase_waitForGPU(lua_State* tolua_S)
+{
+    int argc = 0;
+    ax::rhi::DriverBase* obj = nullptr;
+    bool ok  = true;
+
+#if _AX_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if _AX_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"axrhi.DriverBase",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    obj = (ax::rhi::DriverBase*)tolua_tousertype(tolua_S,1,0);
+
+#if _AX_DEBUG >= 1
+    if (!obj)
+    {
+        tolua_error(tolua_S,"invalid 'obj' in function 'lua_ax_rhi_DriverBase_waitForGPU'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 0)
+    {
+        if(!ok)
+        {
+            tolua_error(tolua_S,"invalid arguments in function 'lua_ax_rhi_DriverBase_waitForGPU'", nullptr);
+            return 0;
+        }
+        obj->waitForGPU();
+        lua_settop(tolua_S, 1);
+        return 1;
+    }
+    luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d \n", "axrhi.DriverBase:waitForGPU",argc, 0);
+    return 0;
+
+#if _AX_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_ax_rhi_DriverBase_waitForGPU'.",&tolua_err);
+#endif
+
+    return 0;
+}
 int lua_ax_rhi_DriverBase_getInstance(lua_State* tolua_S)
 {
     int argc = 0;
@@ -3098,6 +3145,7 @@ int lua_register_ax_rhi_DriverBase(lua_State* tolua_S)
         tolua_function(tolua_S,"getMaxTextureUnits",lua_ax_rhi_DriverBase_getMaxTextureUnits);
         tolua_function(tolua_S,"getMaxSamplesAllowed",lua_ax_rhi_DriverBase_getMaxSamplesAllowed);
         tolua_function(tolua_S,"cleanPendingResources",lua_ax_rhi_DriverBase_cleanPendingResources);
+        tolua_function(tolua_S,"waitForGPU",lua_ax_rhi_DriverBase_waitForGPU);
         tolua_function(tolua_S,"getInstance", lua_ax_rhi_DriverBase_getInstance);
         tolua_function(tolua_S,"destroyInstance", lua_ax_rhi_DriverBase_destroyInstance);
     tolua_endmodule(tolua_S);
