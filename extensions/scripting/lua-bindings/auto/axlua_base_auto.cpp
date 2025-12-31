6d682786e72b33ba32bb4a2dcfd6f8f1c1e834cb
halx99 <6977319+halx99@users.noreply.github.com>
2025-12-31 02:22:39 +0000

    Committing luabindings for commit 5e95810

--- extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
+++ extensions/scripting/lua-bindings/auto/axlua_base_auto.cpp
@@ -69673,6 +69673,56 @@ int lua_ax_base_SpriteFrameCache_getSpriteSheetLoader(lua_State* tolua_S)
 
     return 0;
 }
+int lua_ax_base_SpriteFrameCache_getSpriteSheet(lua_State* tolua_S)
+{
+    int argc = 0;
+    ax::SpriteFrameCache* obj = nullptr;
+    bool ok  = true;
+
+#if _AX_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if _AX_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"ax.SpriteFrameCache",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    obj = (ax::SpriteFrameCache*)tolua_tousertype(tolua_S,1,0);
+
+#if _AX_DEBUG >= 1
+    if (!obj)
+    {
+        tolua_error(tolua_S,"invalid 'obj' in function 'lua_ax_base_SpriteFrameCache_getSpriteSheet'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 1)
+    {
+        std::string_view arg0;
+
+        ok &= luaval_to_std_string_view(tolua_S, 2,&arg0, "ax.SpriteFrameCache:getSpriteSheet");
+        if(!ok)
+        {
+            tolua_error(tolua_S,"invalid arguments in function 'lua_ax_base_SpriteFrameCache_getSpriteSheet'", nullptr);
+            return 0;
+        }
+        auto&& ret = obj->getSpriteSheet(arg0);
+        object_to_luaval<std::shared_ptr<ax::SpriteSheet>>(tolua_S, "std::shared_ptr<ax::SpriteSheet>",(std::shared_ptr<ax::SpriteSheet>)ret);
+        return 1;
+    }
+    luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d \n", "ax.SpriteFrameCache:getSpriteSheet",argc, 1);
+    return 0;
+
+#if _AX_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_ax_base_SpriteFrameCache_getSpriteSheet'.",&tolua_err);
+#endif
+
+    return 0;
+}
 int lua_ax_base_SpriteFrameCache_getInstance(lua_State* tolua_S)
 {
     int argc = 0;
@@ -69772,6 +69822,7 @@ int lua_register_ax_base_SpriteFrameCache(lua_State* tolua_S)
         tolua_function(tolua_S,"addSpriteFrameCapInset",lua_ax_base_SpriteFrameCache_addSpriteFrameCapInset);
         tolua_function(tolua_S,"deregisterSpriteSheetLoader",lua_ax_base_SpriteFrameCache_deregisterSpriteSheetLoader);
         tolua_function(tolua_S,"getSpriteSheetLoader",lua_ax_base_SpriteFrameCache_getSpriteSheetLoader);
+        tolua_function(tolua_S,"getSpriteSheet",lua_ax_base_SpriteFrameCache_getSpriteSheet);
         tolua_function(tolua_S,"getInstance", lua_ax_base_SpriteFrameCache_getInstance);
         tolua_function(tolua_S,"destroyInstance", lua_ax_base_SpriteFrameCache_destroyInstance);
     tolua_endmodule(tolua_S);
