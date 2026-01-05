b5240acfbce68e227e82d02f76764035a9a8ad3b
halx99 <halx99@live.com>
2026-01-05 21:39:49 +0800

    Fix indent [skip ci]

--- axmol/platform/winrt/xaml/AxmolRenderer.cpp
+++ axmol/platform/winrt/xaml/AxmolRenderer.cpp
@@ -45,8 +45,7 @@ AxmolRenderer::AxmolRenderer(int width,
                              CoreDispatcher const& dispatcher,
                              Panel const& panel)
     : m_width(width), m_height(height), m_dpi(dpi), m_dispatcher(dispatcher), m_panel(panel), m_orientation(orientation)
-{
-}
+{}
 
 AxmolRenderer::~AxmolRenderer() {}
 
