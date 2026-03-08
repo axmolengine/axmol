32c7b5c87dc8cee3d7b578371db552064cb9a2ed
halx99 <halx99@live.com>
2026-03-08 14:59:50 +0800

    Update comment [skip ci]

--- extensions/ImGui/src/ImGui/backends/imgui_impl_axmol_sw.h
+++ extensions/ImGui/src/ImGui/backends/imgui_impl_axmol_sw.h
@@ -2,7 +2,7 @@
 #include "imgui_impl_axmol.h"
 #include "axmol/platform/RenderView.h"
 
-/// ImGui SingleWindow system APIs
+/// ImGui Axmol SingleWindow platform spec APIs
 IMGUI_IMPL_API bool ImGui_ImplAxmolSW_InitForAxmol(ax::RenderView* window, bool install_callbacks);
 IMGUI_IMPL_API void ImGui_ImplAxmolSW_Shutdown();
 IMGUI_IMPL_API void ImGui_ImplAxmolSW_NewFrame();
