44cf69c243f5593380c25692d97541f2ed7a292f
halx99 <halx99@live.com>
2026-04-09 00:48:22 +0800

    fixup

--- axmol/platform/winrt/RenderViewImpl-winrt.cpp
+++ axmol/platform/winrt/RenderViewImpl-winrt.cpp
@@ -566,7 +566,8 @@ void RenderViewImpl::handleWindowResized()
 void RenderViewImpl::updateRenderScale()
 {
     if (!rhi::DriverContext::isOpenGL())
-        _renderScale = m_dpi / 96.0f;
+        _renderScale =
+            Application::getContextAttrs().renderScaleMode == RenderScaleMode::Physical ? m_dpi / 96.0f : 1.0f;
     else
         _renderScale = 1.0f;
 }
