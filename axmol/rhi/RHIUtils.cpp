e328be6b8c2535b2092ad894eddd2bfa7d517324
halx99 <halx99@live.com>
2026-04-23 19:45:27 +0800

    code style [skip ci]

--- axmol/rhi/RHIUtils.cpp
+++ axmol/rhi/RHIUtils.cpp
@@ -118,8 +118,10 @@ uint32_t computeRowPitch(PixelFormat format, uint32_t width)
     return 0;
 }
 
-uint32_t computeDataSize(PixelFormat format, int width, int height)
+uint32_t computeDataSize(PixelFormat format, uint32_t width, uint32_t height)
 {
+    if (AX_UNLIKELY(format >= PixelFormat::COUNT))
+        return 0;
     const auto& desc = s_pixelFormatDescriptors[(uint32_t)format];
 
     uint32_t numBlocksX = (width + desc.blockWidth - 1) / desc.blockWidth;
