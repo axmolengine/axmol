193cab7c869f15d524a67fbec6b58fd0a98357ca
Patrick Felixberger <robomaniac8@googlemail.com>
2026-05-10 17:01:50 +0200

    Removed unused code

--- axmol/2d/FastTMXLayer.cpp
+++ axmol/2d/FastTMXLayer.cpp
@@ -630,8 +630,6 @@ void FastTMXLayer::updateTotalQuads()
         Vec2 tileSize;
         Vec2 tileOffset;
         Vec2 texSize;
-        float ptx;
-        float pty;
         Color32 color;
     };
     std::vector<BatchCache> batchCache(_batches.size());
@@ -651,8 +649,6 @@ void FastTMXLayer::updateTotalQuads()
         batchCache[i] = {AX_SIZE_PIXELS_TO_POINTS(ts->_tileSize),
                          Vec2(ts->_tileOffset.x / csf, -ts->_tileOffset.y / csf),
                          ts->_imageSize,
-                         0.0f,
-                         0.0f,
                          c};
     }
 
@@ -754,10 +750,10 @@ void FastTMXLayer::updateTotalQuads()
             const float ubottom = tileTexture.origin.y / bc.texSize.height;
             const float utop    = ubottom + tileTexture.size.height / bc.texSize.height;
 
-            quad.bl.texCoord = {uleft + bc.ptx, ubottom + bc.pty};
-            quad.br.texCoord = {uright - bc.ptx, ubottom + bc.pty};
-            quad.tl.texCoord = {uleft + bc.ptx, utop - bc.pty};
-            quad.tr.texCoord = {uright - bc.ptx, utop - bc.pty};
+            quad.bl.texCoord = {uleft,  ubottom};
+            quad.br.texCoord = {uright, ubottom};
+            quad.tl.texCoord = {uleft,  utop};
+            quad.tr.texCoord = {uright, utop};
 
             quad.bl.color = quad.br.color = quad.tl.color = quad.tr.color = bc.color;
 
