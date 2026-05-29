d15bf0e1621b618f07b1669416e358d2a8318702
halx99 <6977319+halx99@users.noreply.github.com>
2026-05-29 02:35:06 +0000

    Committing luabindings for commit 3d9ce12

--- extensions/scripting/lua-bindings/auto/axlua_ui_auto.cpp
+++ extensions/scripting/lua-bindings/auto/axlua_ui_auto.cpp
@@ -4390,6 +4390,53 @@ int lua_ax_ui_Widget_dispatchFocusEvent(lua_State* tolua_S)
 
     return 0;
 }
+int lua_ax_ui_Widget_getBrightStyle(lua_State* tolua_S)
+{
+    int argc = 0;
+    ax::ui::Widget* obj = nullptr;
+    bool ok  = true;
+
+#if _AX_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if _AX_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"axui.Widget",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    obj = (ax::ui::Widget*)tolua_tousertype(tolua_S,1,0);
+
+#if _AX_DEBUG >= 1
+    if (!obj)
+    {
+        tolua_error(tolua_S,"invalid 'obj' in function 'lua_ax_ui_Widget_getBrightStyle'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 0)
+    {
+        if(!ok)
+        {
+            tolua_error(tolua_S,"invalid arguments in function 'lua_ax_ui_Widget_getBrightStyle'", nullptr);
+            return 0;
+        }
+        int ret = (int)obj->getBrightStyle();
+        tolua_pushnumber(tolua_S,(lua_Number)ret);
+        return 1;
+    }
+    luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d \n", "axui.Widget:getBrightStyle",argc, 0);
+    return 0;
+
+#if _AX_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_ax_ui_Widget_getBrightStyle'.",&tolua_err);
+#endif
+
+    return 0;
+}
 int lua_ax_ui_Widget_create(lua_State* tolua_S)
 {
     int argc = 0;
@@ -4616,6 +4663,7 @@ int lua_register_ax_ui_Widget(lua_State* tolua_S)
         tolua_function(tolua_S,"propagateTouchEvent",lua_ax_ui_Widget_propagateTouchEvent);
         tolua_function(tolua_S,"onFocusChange",lua_ax_ui_Widget_onFocusChange);
         tolua_function(tolua_S,"dispatchFocusEvent",lua_ax_ui_Widget_dispatchFocusEvent);
+        tolua_function(tolua_S,"getBrightStyle",lua_ax_ui_Widget_getBrightStyle);
         tolua_function(tolua_S,"create", lua_ax_ui_Widget_create);
         tolua_function(tolua_S,"getCurrentFocusedWidget", lua_ax_ui_Widget_getCurrentFocusedWidget);
         tolua_function(tolua_S,"enableDpadNavigation", lua_ax_ui_Widget_enableDpadNavigation);
