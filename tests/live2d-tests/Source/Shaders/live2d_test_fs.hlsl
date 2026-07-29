3b1e776f9209bae3998d10ef07cdfd66dc98e4a0
halx99 <halx99@live.com>
2026-07-30 00:20:10 +0800

    Fix live2d test shader variable declarations

--- tests/live2d-tests/Source/Shaders/live2d_test_fs.hlsl
+++ tests/live2d-tests/Source/Shaders/live2d_test_fs.hlsl
@@ -4,11 +4,11 @@ struct PS_IN {
     float2 v_texCoord : TEXCOORD0;
 };
 
-cbuffer fs_ub : register(b1, space0) {
+cbuffer fs_ub {
     float4 baseColor;
 };
 
-Texture2D u_tex0 : register(t0, space1);
+Texture2D u_tex0;
 
 float4 main(PS_IN input) : SV_Target0 {
     float4 c = u_tex0.Sample(LinearClamp, input.v_texCoord) * baseColor;
